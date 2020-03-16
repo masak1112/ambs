dc6460eeca4f5ad8c1b27c79b41fcb7733c87984
Bing Gong <b.gong@fz-juelich.de>
2020-03-16 13:35:33 +0000

    Update modules.sh

--- env_setup/modules.sh
+++ env_setup/modules.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 module --force purge
-module /usr/local/software/jureca/OtherStages
-#module load Stages/2019a
-#module load Intel/2019.3.199-GCC-8.3.0  ParaStationMPI/5.2.2-1
-#module load mpi4py/3.0.1-Python-3.6.8
+module use $OTHERSTAGES
 module load Stages/2019a
 module load GCCcore/.8.3.0
 module load mpi4py/3.0.1-Python-3.6.8
