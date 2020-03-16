e2702bdac1f8d08857730194cd702d1c7fe28b3b
Bing Gong <b.gong@fz-juelich.de>
2020-03-16 13:52:00 +0000

    Update modules.sh

--- env_setup/modules.sh
+++ env_setup/modules.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-module --force purge
+module purge
 module use $OTHERSTAGES
 module load Stages/2019a
+module load GCC/8.3.0
+module load MVAPICH2/.2.3.1-GDR
 module load GCCcore/.8.3.0
 module load mpi4py/3.0.1-Python-3.6.8
 module load h5py/2.9.0-serial-Python-3.6.8
