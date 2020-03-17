5c2112f3f55891ab74a9475afe082ac14800430a
Bing Gong <gongbing1112@gmail.com>
2020-03-17 10:18:36 +0100

    update workflow

--- bash/workflow_era5.sh
+++ bash/workflow_era5.sh
@@ -2,16 +2,16 @@
 set -e
 
 MODEL=savp
-#train_mode: end_to_end, pre_trained, fine_tune
+#train_mode: end_to_end, pre_trained
 TRAIN_MODE=end_to_end
 EXP_NAME=era5_size_64_64_3_3t_norm
 
+
 DATA_ETL_DIR=/p/scratch/deepacf/{USER}/
-DATA_EXTRA=${DATA_ETL_DIR}/extractedData/${EXP_NAME}
-DATA_PREPROCESS=${DATA_ETL_DIR}/preprocessedData/${EXP_NAME}
-DATA_PREPROCESS_TF=./data/${EXP_NAME}
-TRAIN_OUTPUT=./logs/${EXP_NAME}/${TRAIN_MODE}
-RESULTS_OUTPUT=./results_test_samples/${EXP_NAME}/${TRAIN_MODE}/
+DATA_EXTRA_DIR=${DATA_ETL_DIR}/extractedData/${EXP_NAME}
+DATA_PREPROCESS_DIR=${DATA_ETL_DIR}/preprocessedData/${EXP_NAME}
+DATA_PREPROCESS_TF_DIR=./data/${EXP_NAME}
+RESULTS_OUTPUT_DIR=./results_test_samples/${EXP_NAME}/${TRAIN_MODE}/
 
 if [ $MODEL==savp ]
 then
@@ -27,24 +27,41 @@ else
     exit 1
 fi
 
+if [ $TRAIN_MODE==pre_trained ]; then
+    TRAIN_OUTPUT_DIR=./pretrained_models/kth/${method_dir}
+else
+    TRAIN_OUTPUT_DIR=./logs/${EXP_NAME}/${TRAIN_MODE}
 
 ##############Datat Preprocessing################
 #To hkl data
-python ../workflow_video_prediction/DataPreprocess/benchmark/mpi_stager_v2_process_netCDF.py \
---input_dir $DATA_EXTRA --destination_dir $DATA_PREPROCESS
+if [ -d $DATA_PREPROCESS_DIR ]; then
+    echo "The Preprocessed Data (.hkl ) exist"
+else
+    python ../workflow_video_prediction/DataPreprocess/benchmark/mpi_stager_v2_process_netCDF.py \
+    --input_dir $DATA_EXTRA_DIR --destination_dir $DATA_PREPROCESS_DIR
+fi
+
 #Change the .hkl data to .tfrecords files
-python ./video_prediction/datasets/era5_dataset_v2.py  --source_dir $DATA_PREPROCESS/splits \
---destination_dir $DATA_PREPROCESS_TF
+if [ -d $DATA_PREPROCESS_TF_DIR ]; then
+    echo "The Preprocessed Data (tf.records) exist"
+else
+    python ./video_prediction/datasets/era5_dataset_v2.py  --source_dir $DATA_PREPROCESS_DIR/splits \
+    --destination_dir $DATA_PREPROCESS_TF_DIR
+fi
 
 #########Train##########################
-python ./scripts/train_v2.py --input_dir $DATA_PREPROCESS_TF --dataset era5  \
---model $MODEL --model_hparams_dict hparams/kth/${method_dir}/model_hparams.json \
---output_dir ${TRAIN_OUTPUT}
 
+if [ $TRAIN_MODE==pre_trained ]; then
+    echo "Using kth trained model "
+else
+    python ./scripts/train_v2.py --input_dir $DATA_PREPROCESS_TF_DIR --dataset era5  \
+    --model $MODEL --model_hparams_dict hparams/kth/${method_dir}/model_hparams.json \
+    --output_dir ${TRAIN_OUTPUT_DIR}
+fi
 
 #########Generate results#################
-python ./scripts/generate_transfer_learning_finetune.py --input_dir ${DATA_PREPROCESS_TF} \
---dataset_hparams sequence_length=20 --checkpoint $TRAIN_OUTPUT\
---mode test --results_dir ${RESULTS_OUTPUT} \
+python ./scripts/generate_transfer_learning_finetune.py --input_dir ${DATA_PREPROCESS_TF_DIR} \
+--dataset_hparams sequence_length=20 --checkpoint $TRAIN_OUTPUT_DIR\
+--mode test --results_dir ${RESULTS_OUTPUT_DIR} \
 --batch_size 4 --dataset era5
 
