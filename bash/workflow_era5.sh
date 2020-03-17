2efff51bfe3c7ee3b83517bce293c6c0fa3bb484
Bing Gong <gongbing1112@gmail.com>
2020-03-17 09:57:53 +0100

    update workflow

--- bash/workflow_era5.sh
+++ bash/workflow_era5.sh
@@ -1,50 +1,50 @@
 #!/usr/bin/env bash
 set -e
 
-model="savp"
+MODEL=savp
 #train_mode: end_to_end, pre_trained, fine_tune
-train_mode="end_to_end"
-is_pretrain=False
-exp_name="era5_size_64_64_3_3t_norm"
+TRAIN_MODE=end_to_end
+EXP_NAME=era5_size_64_64_3_3t_norm
 
-if [ $model=="savp" ]
+DATA_ETL_DIR=/p/scratch/deepacf/{USER}/
+DATA_EXTRA=${DATA_ETL_DIR}/extractedData/${EXP_NAME}
+DATA_PREPROCESS=${DATA_ETL_DIR}/preprocessedData/${EXP_NAME}
+DATA_PREPROCESS_TF=./data/${EXP_NAME}
+TRAIN_OUTPUT=./logs/${EXP_NAME}/${TRAIN_MODE}
+RESULTS_OUTPUT=./results_test_samples/${EXP_NAME}/${TRAIN_MODE}/
+
+if [ $MODEL==savp ]
 then
-    method_dir="ours_savp"
-elif [ $model=="gan" ]
+    method_dir=ours_savp
+elif [ $MODEL==gan ]
 then
-    method_dir="ours_gan"
-elif [ $model=="vae" ]
+    method_dir=ours_gan
+elif [ $MODEL==vae ]
 then
-    method_dir="ours_vae"
+    method_dir=ours_vae
 else
     echo "model does not exist" 2>&1
     exit 1
 fi
 
-raw_dataset_input=./${exp_name}
-prep_data_input=./data/${exp_name}
-train_output=./logs/${exp_name}
-results_output=./results_test_samples/${exp_name}/${method_dir}
 
 ##############Datat Preprocessing################
-#For parallel on HPC
+#To hkl data
 python ../workflow_video_prediction/DataPreprocess/benchmark/mpi_stager_v2_process_netCDF.py \
---input_dir "/p/scratch/deepacf/bing/extractedData/" --destination_dir ${raw_dataset_input}
-
-#On local machine
-#python ../worklflow_video_prediction/DataPreprocess/process_netCDF_v2.py
-#Change the .hkl data to .tfrecords data
-python ./video_prediction/datasets/era5_dataset_v2.py  ${raw_dataset_input}/splits ${prep_data_input}
+--input_dir $DATA_EXTRA --destination_dir $DATA_PREPROCESS
+#Change the .hkl data to .tfrecords files
+python ./video_prediction/datasets/era5_dataset_v2.py  --source_dir $DATA_PREPROCESS/splits \
+--destination_dir $DATA_PREPROCESS_TF
 
 #########Train##########################
-python ./scripts/train_v2.py --input_dir ${prep_data_input} --dataset era5  \
---model savp --model_hparams_dict hparams/kth/${method_dir}/model_hparams.json \
---output_dir logs/era5_64_64_3_3t_norm/${train_mode}/${method_dir}\
+python ./scripts/train_v2.py --input_dir $DATA_PREPROCESS_TF --dataset era5  \
+--model $MODEL --model_hparams_dict hparams/kth/${method_dir}/model_hparams.json \
+--output_dir ${TRAIN_OUTPUT}
 
-#--checkpoint pretrained_models/kth/ours_savp
+
 #########Generate results#################
-python ./scripts/generate_transfer_learning_finetune.py --input_dir ${prep_data_input} \
---dataset_hparams sequence_length=20 --checkpoint ${train_output}\
---mode test --results_dir ${results_output} \
+python ./scripts/generate_transfer_learning_finetune.py --input_dir ${DATA_PREPROCESS_TF} \
+--dataset_hparams sequence_length=20 --checkpoint $TRAIN_OUTPUT\
+--mode test --results_dir ${RESULTS_OUTPUT} \
 --batch_size 4 --dataset era5
 
