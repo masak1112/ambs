e74bea7452581d01dd4a5a8912a51d8990c41e1b
Bing Gong <gongbing1112@gmail.com>
2020-03-18 10:30:17 +0100

    add env func

--- env_setup/create_env.sh
+++ env_setup/create_env.sh
@@ -6,28 +6,34 @@ if [[ ! -n "$1" ]]; then
 fi
 
 if [[ ! -n "$2" ]]; then
-  echo "Provide the user name, which will be taken as folder name"
+  echo "Provide the env name, which will be taken as folder name"
   exit 1
 fi
 
-env_name=$2
+ENV_NAME=$2
 FOLDER_NAME=$1
 WORKING_DIR=/p/project/deepacf/deeprain/${FOLDER_NAME}/video_prediction_savp
-ENV_DIR=${WORKING_DIR}/env_setup
+ENV_SETUP_DIR=${WORKING_DIR}/env_setup
+ENV_DIR=${WORKING_DIR}/${ENV_NAME}
 
-source ${ENV_DIR}/modules.sh
+
+
+python3 -m venv $ENV_DIR
+source ${ENV_DIR}/bin/activate
+source ${ENV_SETUP_DIR}/modules.sh
 # Install additional Python packages.
-pip3 install --user --ignore-installed -r requirements.txt
+
+pip3 install --ignore-installed -r requirements.txt
 #pip3 install --user netCDF4
 #pip3 install --user numpy
 
 #Copy the hickle package from bing's account
-cd ${ENV_DIR}
-cp  -r /p/project/deepacf/deeprain/bing/hickle .
-export PYTHONPATH=${ENV_DIR}/hickle/lib/python3.6/site-packages:$PYTHONPATH
+cp  -r /p/project/deepacf/deeprain/bing/hickle ${WORKING_DIR}
+export PYTHONPATH=${WORKING_DIR}/hickle/lib/python3.6/site-packages:$PYTHONPATH
 export PYTHONPATH=${WORKING_DIR}:$PYTHONPATH
-export PYTHONPATH=/p/home/jusers/${USER}/juwels/.local/bin:$PYTHONPATH
+export PYTHONPATH=${ENV_DIR}/lib/python3.6/site-packages:$PYTHONPATH
+#export PYTHONPATH=/p/home/jusers/${USER}/juwels/.local/bin:$PYTHONPATH
 export PYTHONPATH=${WORKING_DIR}/lpips-tensorflow:$PYTHONPATH
-jutil env activate -p $2
+#jutil env activate -p $2
 
 
