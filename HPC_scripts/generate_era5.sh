4b14784bd4d0f068ff8af32347c7208b8bfd2c46
Bing Gong <gongbing1112@gmail.com>
2020-03-20 09:26:46 +0100

    add DataExtraction submission job

--- HPC_scripts/generate_era5.sh
+++ HPC_scripts/generate_era5.sh
@@ -20,7 +20,6 @@ module load ParaStationMPI/5.2.2-1
 module load TensorFlow/1.13.1-GPU-Python-3.6.8
 module load netcdf4-python/1.5.0.1-Python-3.6.8
 module load h5py/2.9.0-Python-3.6.8
-source mandarine/bin/activate
 
 python scripts/generate_transfer_learning_finetune.py --input_dir data/era5_size_64_64_3_norm_dup --dataset_hparams sequence_length=20 --checkpoint logs/era5_64_64_3_norm_2016/ours_savp --mode test --results_dir results_test_samples/era5_size_64_64_3_norm_2016  --batch_size 4 --dataset era5 
 #srun  python scripts/train.py --input_dir data/era5 --dataset era5  --model savp --model_hparams_dict hparams/kth/ours_savp/model_hparams.json --output_dir logs/era5/ours_savp
