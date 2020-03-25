77ce76f5f8664e70e127b2e730cf4c22ed0b6626
Bing Gong <gong1@juwels03.ib.juwels.fzj.de>
2020-03-25 10:32:01 +0100

    Update train_era.sh

--- HPC_scripts/train_era5.sh
+++ HPC_scripts/train_era5.sh
@@ -24,5 +24,5 @@ module load cuDNN/7.5.1.10-CUDA-10.1.105
 
 #srun  python scripts/train.py --input_dir data/kth --dataset kth  --model savp --model_hparams_dict hparams/kth/ours_savp/model_hparams.json --output_dir logs/kth/ours_savp
 
-srun python ../scripts/train_v2.py --input_dir ../data/era5_size_64_64_3_3t_norm --dataset era5  --model savp --model_hparams_dict ../hparams/kth/ours_savp/model_hparams.json --output_dir ../logs/era5_size_64_64_3_3t_norm/ours_savp
+srun python ../scripts/train_v2.py --input_dir ../data/era5_size_64_64_3_3t_norm  --dataset era5  --model savp --model_hparams_dict ../hparams/kth/ours_savp/model_hparams.json --output_dir ../logs/era5_size_64_64_3_3t_norm/ours_savp
 #srun  python scripts/train.py --input_dir data/era5 --dataset era5  --model savp --model_hparams_dict hparams/kth/ours_savp/model_hparams.json --output_dir logs/era5/ours_savp
