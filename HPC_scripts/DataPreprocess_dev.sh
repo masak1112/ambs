f522ced5ea782d5b43e4e9257fddd39bcd85d0d0
Bing Gong <b.gong@fz-juelich.de>
2020-03-30 15:26:42 +0000

    Update DataPreprocess_dev.sh

--- HPC_scripts/DataPreprocess_dev.sh
+++ HPC_scripts/DataPreprocess_dev.sh
@@ -18,5 +18,5 @@ module load Intel/2019.3.199-GCC-8.3.0  ParaStationMPI/5.2.2-1
 module load h5py/2.9.0-Python-3.6.8
 module load mpi4py/3.0.1-Python-3.6.8
 
-srun python mpi_stager_v2_process_netCDF.py --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/ \
+srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/ \
 --destination_dir /p/scratch/deepacf/scarlet/processData_size_64_64_3_3t_norm
