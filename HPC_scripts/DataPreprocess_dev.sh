f88fdac98dc80b9a27f8e441995c3e67782512b2
Bing Gong <gong1@juwels02.ib.juwels.fzj.de>
2020-04-07 12:11:31 +0200

    update DataPreprocess_dev.sh

--- HPC_scripts/DataPreprocess_dev.sh
+++ HPC_scripts/DataPreprocess_dev.sh
@@ -19,6 +19,13 @@ module load h5py/2.9.0-Python-3.6.8
 module load mpi4py/3.0.1-Python-3.6.8
 
 srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py \
- --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/ \
- --destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/Y2017M01to12-128_160-74.00N710E-T_MSL_gph500 \
+ --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/2016 \
+ --destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/Y2016toY2017M01to12-128x160-74d00N71d0E-T_MSL_gph500/2016 \
  --vars T2 MSL gph500 --lat_s 74 --lat_e 202 --lon_s 550 --lon_e 710
+
+
+
+#srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py \
+# --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/2017 \
+# --destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/Y2016toY2017M01to12-128x160-74d00N71d0E-T_MSL_gph500/2017 \
+# --vars T2 MSL gph500 --lat_s 74 --lat_e 202 --lon_s 550 --lon_e 710
