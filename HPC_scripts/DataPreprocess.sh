b882724a5d9430184570405d5ae4e1ac2023f889
Bing Gong <gong1@juwels00.ib.juwels.fzj.de>
2020-04-07 21:52:03 +0200

    update DataPreprocess.sh and DataPreprocess_dev.sh related to https://gitlab.version.fz-juelich.de/toar/video_prediction_savp.git/issues/36

--- HPC_scripts/DataPreprocess.sh
+++ HPC_scripts/DataPreprocess.sh
@@ -1,29 +1,43 @@
 #!/bin/bash -x
 #SBATCH --account=deepacf
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-##SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=12
+##SBATCH --ntasks-per-node=12
 #SBATCH --cpus-per-task=1
 #SBATCH --output=DataPreprocess-out.%j
 #SBATCH --error=DataPreprocess-err.%j
-#SBATCH --time=20:00:00
-#SBATCH --gres=gpu:1
-#SBATCH --partition=gpus
+#SBATCH --time=02:20:00
+#SBATCH --partition=batch
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=b.gong@fz-juelich.de
-##jutil env activate -p cjjsc42
 
-module --force purge 
-module use $OTHERSTAGES
+module --force purge
+module use  $OTHERSTAGES
 module load Stages/2019a
-module load GCCcore/.8.3.0
+module load Intel/2019.3.199-GCC-8.3.0  ParaStationMPI/5.2.2-1
+module load h5py/2.9.0-Python-3.6.8
 module load mpi4py/3.0.1-Python-3.6.8
-module load h5py/2.9.0-serial-Python-3.6.8
-module load TensorFlow/1.13.1-GPU-Python-3.6.8
-module load cuDNN/7.5.1.10-CUDA-10.1.105
 
 
-srun python video_prediction/datasets/era5_dataset_v2.py /p/scratch/deepacf/bing/processData_size_64_64_3_2016/splits/ ./data/era5_size_64_64_3_norm_2016
+srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py \
+ --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/2015/ \
+ --destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-T_MSL_gph500/2015/ \
+ --vars T2 MSL gph500 --lat_s 74 --lat_e 202 --lon_s 550 --lon_e 710
 
-#srun python scripts/generate_transfer_learning.py --input_dir data/era5_size_64_64_1_v2 --dataset_hparams sequence_length=20  --checkpoint pretrained_models/kth/ours_savp  --mode test --results_dir results_test_samples/era5_size_64_64_1_v2_pretrained --dataset era5
-#srun  python scripts/train.py --input_dir data/era5 --dataset era5  --model savp --model_hparams_dict hparams/kth/ours_savp/model_hparams.json --output_dir logs/era5/ours_savp
+srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py \
+ --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/2016/ \
+ --destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-T_MSL_gph500/2016/ \
+ --vars T2 MSL gph500 --lat_s 74 --lat_e 202 --lon_s 550 --lon_e 710
+
+srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py \
+ --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/2017/ \
+ --destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-T_MSL_gph500/2017/ \
+ --vars T2 MSL gph500 --lat_s 74 --lat_e 202 --lon_s 550 --lon_e 710
+
+
+
+
+#srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py \
+# --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/2017 \
+# --destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/Y2016toY2017M01to12-128x160-74d00N71d0E-T_MSL_gph500/2017 \
+# --vars T2 MSL gph500 --lat_s 74 --lat_e 202 --lon_s 550 --lon_e 710
--- HPC_scripts/DataPreprocess_dev.sh
+++ HPC_scripts/DataPreprocess_dev.sh
@@ -18,10 +18,42 @@ module load Intel/2019.3.199-GCC-8.3.0  ParaStationMPI/5.2.2-1
 module load h5py/2.9.0-Python-3.6.8
 module load mpi4py/3.0.1-Python-3.6.8
 
-srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py \
- --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/2016 \
- --destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/Y2016toY2017M01to12-128x160-74d00N71d0E-T_MSL_gph500/2016 \
- --vars T2 MSL gph500 --lat_s 74 --lat_e 202 --lon_s 550 --lon_e 710
+
+source_dir=/p/scratch/deepacf/video_prediction_shared_folder/extractedData/
+destination_dir=/p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-T_MSL_gph500/
+years={2015,2016,2017}
+
+for year in $years:
+    do 
+	srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py \
+         --source_dir ${source_dir}/${year}/ \
+         --destination_dir ${destination_dir}/${year}/ --vars T2 MSL gph500 --lat_s 74 --lat_e 202 --lon_s 550 --lon_e 710
+    done
+
+	
+srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_split_data_multi_years.py \
+--destination_dir ${destination_dir}
+
+
+
+
+#srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py \
+# --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/2015/ \
+# --destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-T_MSL_gph500/2015/ \
+# --vars T2 MSL gph500 --lat_s 74 --lat_e 202 --lon_s 550 --lon_e 710
+
+#srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py \
+# --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/2016/ \
+# --destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-T_MSL_gph500/2016/ \
+# --vars T2 MSL gph500 --lat_s 74 --lat_e 202 --lon_s 550 --lon_e 710
+
+#srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_stager_v2_process_netCDF.py \
+# --source_dir /p/scratch/deepacf/video_prediction_shared_folder/extractedData/2017/ \
+# --destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-T_MSL_gph500/2017/ \
+# --vars T2 MSL gph500 --lat_s 74 --lat_e 202 --lon_s 550 --lon_e 710
+
+#srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_split_data_multi_years.py \
+#--destination_dir /p/scratch/deepacf/video_prediction_shared_folder/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-#T_MSL_gph500/
 
 
 
