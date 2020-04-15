5c2f7df7e823038da7a43924987a09e708cfddb7
Bing Gong <gong1@juwels04.ib.juwels.fzj.de>
2020-04-15 14:50:04 +0200

    update mpi_split_data_multi_years

--- DataPreprocess/mpi_split_data_multi_years.py
+++ DataPreprocess/mpi_split_data_multi_years.py
@@ -5,24 +5,21 @@ import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--destination_dir", type=str, default="/p/scratch/deepacf/bing/processData_size_64_64_3_3t_norm")
-parser.add_argument("--partition", type=str, default="")
+#parser.add_argument("--partition", type=str, default="")
 args = parser.parse_args()
 target_dir = args.destination_dir
-partition = args.partition
-partition = json.loads(partition)
-print ("partition", partition)
 
-##partition = {
-##            "train":{
-##                "2016":[1,2,3,4,5,6,7,8,9,10,11,12]
-##                },
-##            "val":
-##                {"2017":[1,2,3,4,5,6,7,8,9,10,11,12]
-##                 },
-##            "test":
-##                {"2015":[1,2,3,4,5,6,7,8,9,10,11,12]
-##                 }
-##            }
+partition = {
+            "train":{
+                "2016":[1,2,3,4,5,6,7,8,9,10,11,12]
+                },
+            "val":
+                {"2017":[1,2,3,4,5,6,7,8,9,10,11,12]
+                 },
+            "test":
+                {"2015":[1,2,3,4,5,6,7,8,9,10,11,12]
+                 }
+            }
 # ini. MPI
 comm = MPI.COMM_WORLD
 my_rank = comm.Get_rank()  # rank of the node
