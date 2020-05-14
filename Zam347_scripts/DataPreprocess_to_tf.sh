13cf5bf0a3795d41acbf3dd988c60a21440af6c8
b.gong <b.gong@fz-juelich.de>
2020-05-14 11:48:33 +0200

    Update vars information for DataPreprocess_to_tf.sh

--- Zam347_scripts/DataPreprocess_to_tf.sh
+++ Zam347_scripts/DataPreprocess_to_tf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
 
-python ../video_prediction/datasets/era5_dataset_v2.py /home/${USER}/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-T_MSL_gph500/hickle/splits/ /home/${USER}/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-T_MSL_gph500/tfrecords/ -vars T2 T2 T2 
+python ../video_prediction/datasets/era5_dataset_v2.py /home/${USER}/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-T_MSL_gph500/hickle/splits/ /home/${USER}/preprocessedData/era5-Y2015toY2017M01to12-128x160-74d00N71d00E-T_MSL_gph500/tfrecords/ -vars T2 MSL gph500 
