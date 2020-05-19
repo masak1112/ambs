5d600c63bac653ac953ae32d780ebea9d4232cf9
michael <m.langguth@fz-juelich.de>
2020-05-19 18:09:19 +0200

    Added varnames to be passed and handled to/in 'mpi_split_data_multi_years.py'

--- DataPreprocess/mpi_split_data_multi_years.py
+++ DataPreprocess/mpi_split_data_multi_years.py
@@ -5,8 +5,10 @@ from process_netCDF_v2 import *
 parser = argparse.ArgumentParser()
 #parser.add_argument("--source_dir", type=str, default="/p/scratch/deepacf/bing/extractedData/")
 parser.add_argument("--destination_dir", type=str, default="/p/scratch/deepacf/bing/processData_size_64_64_3_3t_norm")
+parser.add_argument("--varnames","-vars",dest="varnames", nargs = '+')
 args = parser.parse_args()
 target_dir = args.destination_dir
+varnames = args.varnames
 
 
 partition = {
@@ -25,6 +27,6 @@ comm = MPI.COMM_WORLD
 my_rank = comm.Get_rank()  # rank of the node
 p = comm.Get_size()  # number of assigned nods
 if my_rank == 0:  # node is master
-    split_data_multiple_years(target_dir=target_dir,partition=partition)
+    split_data_multiple_years(target_dir=target_dir,partition=partition,varnames=varnames)
 else:
     pass
