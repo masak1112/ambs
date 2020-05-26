56d0a8f8737d23c2fe4726b0f1adc0488e4712a4
Michael <m.langguth@fz-juelich.de>
2020-05-26 11:52:34 +0200

    Added documentation to normalization class.

--- video_prediction/datasets/era5_dataset_v2.py
+++ video_prediction/datasets/era5_dataset_v2.py
@@ -161,6 +161,10 @@ def save_tf_record(output_fname, sequences):
             writer.write(example.SerializeToString())
             
 class norm_data:
+    """
+     Class for normalizing data. The statistical data for normalization (minimum, maximum, average, standard deviation etc.) is expected to be available from a statistics-dictionary
+     created with the calc_data_stat-class (see 'process_netCDF_v2.py'.
+    """
     
     ### set known norms and the requested statistics (to be retrieved from statistics.json) here ###
     known_norms = {}
@@ -168,20 +172,27 @@ class norm_data:
     known_norms["znorm"]  = ["avg","sigma"]
     
     def __init__(self,varnames):
+        """Initialize the instance by setting the variable names to be handled and the status (for sanity checks only) as attributes."""
         varnames_uni, _, nvars = get_unique_vars(varnames)
         
         self.varnames = varnames_uni
         self.status_ok= False
             
     def check_and_set_norm(self,stat_dict,norm):
+        """
+         Checks if the statistics-dictionary provides the required data for selected normalization method and expands the instance's attributes accordingly.
+         Example: minmax-normalization requires the minimum and maximum value of a variable named var1. 
+                 If the requested values are provided by the statistics-dictionary, the instance gets the attributes 'var1min' and 'var1max',respectively.
+        """
         
-        if not norm in self.known_norms.keys():
+        # some sanity checks
+        if not norm in self.known_norms.keys(): # valid normalization requested?
             print("Please select one of the following known normalizations: ")
             for norm_avail in self.known_norms.keys():
                 print(norm_avail)
             raise ValueError("Passed normalization '"+norm+"' is unknown.")
        
-        if not all(items in stat_dict for items in self.varnames):
+        if not all(items in stat_dict for items in self.varnames): # all variables found in dictionary?
             print("Keys in stat_dict:")
             print(stat_dict.keys())
             
@@ -189,40 +200,48 @@ class norm_data:
             print(self.varnames)
             raise ValueError("Could not find all requested variables in statistics dictionary.")   
 
+        # create all attributes for the instance
         for varname in self.varnames:
             for stat_name in self.known_norms[norm]:
                 setattr(self,varname+stat_name,stat_dict[varname][0][stat_name])
                 
-        self.status_ok = True
+        self.status_ok = True           # set status for normalization -> ready
                 
     def norm_var(self,data,varname,norm):
+        """ 
+         Performs given normalization on input data (given that the instance is already set up)
+        """
         
         # some sanity checks
-        if not self.status_ok: raise ValueError("norm_data-object needs to be initialized and checked first.")
+        if not self.status_ok: raise ValueError("norm_data-object needs to be initialized and checked first.") # status ready?
         
-        if not norm in self.known_norms.keys():
+        if not norm in self.known_norms.keys():                                # valid normalization requested?
             print("Please select one of the following known normalizations: ")
             for norm_avail in self.known_norms.keys():
                 print(norm_avail)
             raise ValueError("Passed normalization '"+norm+"' is unknown.")
         
-        
+        # do the normalization and return
         if norm == "minmax":
             return((data[...] - getattr(self,varname+"min"))/(getattr(self,varname+"max") - getattr(self,varname+"min")))
         elif norm == "znorm":
             return((data[...] - getattr(self,varname+"avg"))/getattr(self,varname+"sigma")**2)
         
     def denorm_var(self,data,varname,norm):
+        """ 
+         Performs given denormalization on input data (given that the instance is already set up), i.e. inverse method to norm_var
+        """
         
         # some sanity checks
-        if not self.status_ok: raise ValueError("norm_data-object needs to be initialized and checked first.")        
+        if not self.status_ok: raise ValueError("norm_data-object needs to be initialized and checked first.") # status ready?        
         
-        if not norm in self.known_norms.keys():
+        if not norm in self.known_norms.keys():                                # valid normalization requested?
             print("Please select one of the following known normalizations: ")
             for norm_avail in self.known_norms.keys():
                 print(norm_avail)
             raise ValueError("Passed normalization '"+norm+"' is unknown.")
         
+        # do the denormalization and return
         if norm == "minmax":
             return(data[...] * (getattr(self,varname+"max") - getattr(self,varname+"min")) + getattr(self,varname+"max"))
         elif norm == "znorm":
@@ -243,10 +262,10 @@ def read_frames_and_save_tf_records(output_dir,input_dir,partition_name,vars_in,
     output_dir = os.path.join(output_dir,partition_name)
     os.makedirs(output_dir,exist_ok=True)
     
-    norm_cls  = norm_data(vars_in)
+    norm_cls  = norm_data(vars_in)       # init normalization-instance
     nvars     = len(vars_in)
     
-    # open statistics file and store the dictionary
+    # open statistics file and feed it to norm-instance
     with open(os.path.join(input_dir,"statistics.json")) as js_file:
         norm_cls.check_and_set_norm(json.load(js_file),norm)        
     
