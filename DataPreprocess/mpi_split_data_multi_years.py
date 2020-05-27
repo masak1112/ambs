54f8a4ba3b0656080236f7f7dd57586bc626bb41
b.gong <b.gong@fz-juelich.de>
2020-05-27 11:17:51 +0200

    mpi_split_data_mulit_years

--- DataPreprocess/mpi_split_data_multi_years.py
+++ DataPreprocess/mpi_split_data_multi_years.py
@@ -6,28 +6,28 @@ import json
 #add parser arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--destination_dir","-dest",dest="destination_dir",type=str, default="/p/scratch/deepacf/bing/processData_size_64_64_3_3t_norm")
-parser.add_argument("--partition","-part",dest="partition",type=json.loads)
+#parser.add_argument("--partition","-part",dest="partition",type=json.loads)
 #                    help="--partition allows to control the splitting of the processed data in training, test and validation data. Pass a dictionary-like string.")
 #parser.add_argument("--partition", type=str, default="")
 args = parser.parse_args()
 target_dir = args.destination_dir
 
-partition = args.partition
-all_keys  = partition.keys()
-for key in all_keys:
-    print(partition[key]) 
+#partition = args.partition
+#all_keys  = partition.keys()
+#for key in all_keys:
+#    print(partition[key]) 
 
-#partition = {
-#            "train":{
-#                "2016":[1,2,3,4,5,6,7,8,9,10,11,12]
-#                },
-#            "val":
-#                {"2017":[1,2,3,4,5,6,7,8,9,10,11,12]
-#                 },
-#            "test":
-#                {"2015":[1,2,3,4,5,6,7,8,9,10,11,12]
-#                 }
-#            }
+partition = {
+            "train":{
+                "2017":[1]
+               },
+            "val":
+                {"2017":[2]
+                 },
+            "test":
+                {"2017":[2]
+                 }
+            }
 # ini. MPI
 comm = MPI.COMM_WORLD
 my_rank = comm.Get_rank()  # rank of the node
