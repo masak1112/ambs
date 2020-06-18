29d6120e813a04eff64ab4955f8dbcbd09105a1c
amirpasha <a.mozaffari@fz-juelich.de>
2020-06-18 11:40:45 +0200

    hot-fix for path variable decleration in DataPreprocess_to_tf.sh

--- Zam347_scripts/DataPreprocess_to_tf.sh
+++ Zam347_scripts/DataPreprocess_to_tf.sh
@@ -5,4 +5,4 @@ source_dir=/home/${USER}/preprocessedData/
 destination_dir=/home/${USER}/preprocessedData/
 
 
-python ../video_prediction/datasets/era5_dataset_v2.py ${source_dir} ${destination_dir} -vars T2 MSL gph500 -height 128 -width 160 -seq_length 20 
+python ../video_prediction/datasets/era5_dataset_v2.py ${source_dir}/splits/hickle ${destination_dir}/tf_records -vars T2 MSL gph500 -height 128 -width 160 -seq_length 20 
