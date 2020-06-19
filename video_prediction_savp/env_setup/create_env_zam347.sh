1c91ffbf6ed35b3cc638a0ec4048c8ed0560d074
Bing Gong <b.gong@fz-juelich.de>
2020-06-19 09:15:51 +0200

    create folder external_package and mv hickle and lpips-tensorflow  package there

--- video_prediction_savp/env_setup/create_env_zam347.sh
+++ video_prediction_savp/env_setup/create_env_zam347.sh
@@ -7,34 +7,26 @@ if [[ ! -n "$1" ]]; then
 fi
 
 ENV_NAME=$1
-WORKING_DIR=/home/$USER/video_prediction_savp
+WORKING_DIR=/home/$USER/ambs/video_prediction_savp
 ENV_SETUP_DIR=${WORKING_DIR}/env_setup
 ENV_DIR=${WORKING_DIR}/${ENV_NAME}
 unset PYTHONPATH
-#source ${ENV_SETUP_DIR}/modules.sh
 # Install additional Python packages.
 python3 -m venv $ENV_DIR
 source ${ENV_DIR}/bin/activate
 pip3 install --upgrade pip
 pip3 install -r ${ENV_SETUP_DIR}/requirements.txt
-#conda install mpi4py
 pip3 install  mpi4py 
 pip3 install netCDF4
 pip3 install  numpy
 pip3 install h5py
 pip3 install tensorflow-gpu==1.13.1
-#Copy the hickle package from bing's account
-#cp  -r /p/project/deepacf/deeprain/bing/hickle ${WORKING_DIR}
-cp -r /home/b.gong/video_prediction_savp/hickle ${WORKING_DIR}
-
-#source ${ENV_SETUP_DIR}/modules.sh
-#source ${ENV_DIR}/bin/activate
 
 #export PYTHONPATH=/home/$USER/miniconda3/pkgs:$PYTHONPATH
-export PYTHONPATH=${WORKING_DIR}/hickle/lib/python3.6/site-packages:$PYTHONPATH
+export PYTHONPATH=${WORKING_DIR}/external_package/hickle/lib/python3.6/site-packages:$PYTHONPATH
 export PYTHONPATH=${WORKING_DIR}:$PYTHONPATH
 #export PYTHONPATH=${ENV_DIR}/lib/python3.6/site-packages:$PYTHONPATH
 #export PYTHONPATH=/p/home/jusers/${USER}/juwels/.local/bin:$PYTHONPATH
-export PYTHONPATH=${WORKING_DIR}/lpips-tensorflow:$PYTHONPATH
+export PYTHONPATH=${WORKING_DIR}/external_package/lpips-tensorflow:$PYTHONPATH
 
 
