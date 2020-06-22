58bf0c6a64c4e53a01fc3d7cd5b0040a84aa87b5
Bing Gong <gong1@juwels00.ib.juwels.fzj.de>
2020-06-22 11:18:17 +0200

    automatically generate email for users on JUWELS sbatch scripts through env set up process #3

--- video_prediction_savp/env_setup/create_env.sh
+++ video_prediction_savp/env_setup/create_env.sh
@@ -12,9 +12,14 @@ fi
 
 ENV_NAME=$2
 FOLDER_NAME=$1
-WORKING_DIR=/p/project/deepacf/deeprain/${FOLDER_NAME}/video_prediction_savp
-ENV_SETUP_DIR=${WORKING_DIR}/env_setup
+ENV_SETUP_DIR=`pwd`
+WORKING_DIR="$(dirname "$ENV_SETUP_DIR")"
 ENV_DIR=${WORKING_DIR}/${ENV_NAME}
+USER_EMAIL=$(jutil user show -o json | grep email | cut -f2 -d':' | cut -f1 -d',' | cut -f2 -d'"')
+echo $USER_EMAIL
+
+#replace the email in sbatch script with the USER_EMAIL
+sed -i "s/--mail-user=.*/--mail-user=$USER_EMAIL/g" ../HPC_scripts/*.sh
 
 source ${ENV_SETUP_DIR}/modules.sh
 # Install additional Python packages.
@@ -24,16 +29,13 @@ pip3 install -r ${ENV_SETUP_DIR}/requirements.txt
 #pip3 install --user netCDF4
 #pip3 install --user numpy
 
-#Copy the hickle package from bing's account
-cp  -r /p/project/deepacf/deeprain/bing/hickle ${WORKING_DIR}
-
 source ${ENV_SETUP_DIR}/modules.sh
 source ${ENV_DIR}/bin/activate
 
-export PYTHONPATH=${WORKING_DIR}/hickle/lib/python3.6/site-packages:$PYTHONPATH
+export PYTHONPATH=${WORKING_DIR}/external_package/hickle/lib/python3.6/site-packages:$PYTHONPATH
 export PYTHONPATH=${WORKING_DIR}:$PYTHONPATH
 export PYTHONPATH=${ENV_DIR}/lib/python3.6/site-packages:$PYTHONPATH
 #export PYTHONPATH=/p/home/jusers/${USER}/juwels/.local/bin:$PYTHONPATH
-export PYTHONPATH=${WORKING_DIR}/lpips-tensorflow:$PYTHONPATH
+export PYTHONPATH=${WORKING_DIR}/external_package/lpips-tensorflow:$PYTHONPATH
 
 
