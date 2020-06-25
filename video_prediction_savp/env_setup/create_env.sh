0c74fadb6007931ca655fabdbad5e9d74efdd20d
Michael <m.langguth@fz-juelich.de>
2020-06-25 12:45:48 +0200

    Generalisation of virtual env generation for different systems (Juwels, HDFML and zam347).

--- video_prediction_savp/env_setup/create_env.sh
+++ video_prediction_savp/env_setup/create_env.sh
@@ -1,41 +1,65 @@
 #!/usr/bin/env bash
 
 if [[ ! -n "$1" ]]; then
-  echo "Provide the user name, which will be taken as folder name"
+  echo "Provide a name to set up the virtual environment"
   exit 1
 fi
 
-if [[ ! -n "$2" ]]; then
-  echo "Provide the env name, which will be taken as folder name"
-  exit 1
-fi
-
-ENV_NAME=$2
-FOLDER_NAME=$1
+HOST_NAME=`hostname`
+ENV_NAME=$1
 ENV_SETUP_DIR=`pwd`
 WORKING_DIR="$(dirname "$ENV_SETUP_DIR")"
+EXE_DIR="$(basename "$ENV_SETUP_DIR")"
 ENV_DIR=${WORKING_DIR}/${ENV_NAME}
 USER_EMAIL=$(jutil user show -o json | grep email | cut -f2 -d':' | cut -f1 -d',' | cut -f2 -d'"')
-echo $USER_EMAIL
+
+#sanity check -> ensure execution from env_setup-directory
+if [[ "${EXE_DIR}" != "env_setup"  ]]; then
+  echo "Please execute the setup-script for the virtual environment from the env_setup-directory!"
+  exit 1
+fi
 
 #replace the email in sbatch script with the USER_EMAIL
 sed -i "s/--mail-user=.*/--mail-user=$USER_EMAIL/g" ../HPC_scripts/*.sh
 
-source ${ENV_SETUP_DIR}/modules.sh
-# Install additional Python packages.
+if [[ "${HOST_NAME}" == hdfml* || "${HOST_NAME}" == juwels* ]]; then
+    # check module availability for the first time on known HPC-systems
+    source ${ENV_SETUP_DIR}/modules.sh
+elif [[ "${HOST_NAME}" == "zam347" ]]; then
+    unset PYTHONPATH
+fi
+
+# Activate virtual environmen and install additional Python packages.
+echo "Configuring and activating virtual environment on ${HOST_NAME}"
+    
 python3 -m venv $ENV_DIR
 source ${ENV_DIR}/bin/activate
-pip3 install -r ${ENV_SETUP_DIR}/requirements.txt
-#pip3 install --user netCDF4
-#pip3 install --user numpy
+if [[ "${HOST_NAME}" == hdfml* || "${HOST_NAME}" == juwels* ]]; then
+    # check module availability for the first time on known HPC-systems
+    pip3 install -r ${ENV_SETUP_DIR}/requirements.txt
+    #pip3 install --user netCDF4
+    #pip3 install --user numpy
+    
+    source ${ENV_SETUP_DIR}/modules.sh
+    source ${ENV_DIR}/bin/activate
+elif [[ "${HOST_NAME}" == "zam347" ]]; then
+    pip3 install --upgrade pip
+    pip3 install -r ${ENV_SETUP_DIR}/requirements.txt
+    pip3 install  mpi4py 
+    pip3 install netCDF4
+    pip3 install  numpy
+    pip3 install h5py
+    pip3 install tensorflow-gpu==1.13.1
+fi
 
-source ${ENV_SETUP_DIR}/modules.sh
-source ${ENV_DIR}/bin/activate
-
+# expand environmental variable PYTHONPATH 
 export PYTHONPATH=${WORKING_DIR}/external_package/hickle/lib/python3.6/site-packages:$PYTHONPATH
 export PYTHONPATH=${WORKING_DIR}:$PYTHONPATH
-export PYTHONPATH=${ENV_DIR}/lib/python3.6/site-packages:$PYTHONPATH
 #export PYTHONPATH=/p/home/jusers/${USER}/juwels/.local/bin:$PYTHONPATH
 export PYTHONPATH=${WORKING_DIR}/external_package/lpips-tensorflow:$PYTHONPATH
+
+if [[ "${HOST_NAME}" == hdfml* || "${HOST_NAME}" == juwels* ]]; then
+    export PYTHONPATH=${ENV_DIR}/lib/python3.6/site-packages:$PYTHONPATH
+fi
 
 
