de1b637a2569734fd39c510f3b80142808bf3524
gong1 <b.gong@fz-juelich.de>
2020-06-26 16:42:21 +0200

    clean train_dummy.py

--- video_prediction_savp/scripts/train_dummy.py
+++ video_prediction_savp/scripts/train_dummy.py
@@ -23,6 +23,127 @@ def add_tag_suffix(summary, tag_suffix):
         value.tag = '/'.join([tag_split[0] + tag_suffix] + tag_split[1:])
     return summary.SerializeToString()
 
+def generate_output_dir(output_dir, model,model_hparams,logs_dir,output_dir_postfix):
+   if output_dir is None:
+        list_depth = 0
+        model_fname = ''
+        for t in ('model=%s,%s' % (model, model_hparams)):
+            if t == '[':
+                list_depth += 1
+            if t == ']':
+                list_depth -= 1
+            if list_depth and t == ',':
+                t = '..'
+            if t in '=,':
+                t = '.'
+            if t in '[]':
+                t = ''
+            model_fname += t
+        output_dir = os.path.join(logs_dir, model_fname) + output_dir_postfix
+    return output_dir
+
+
+def get_model_hparams_dict(model_hparams_dict):
+    """
+    Get model_hparams_dict from json file
+    """
+    model_hparams_dict_load = {}
+    if model_hparams_dict:
+        with open(model_hparams_dict) as f:
+            model_hparams_dict_load.update(json.loads(f.read()))
+   return model_hparams_dict
+
+def resume_checkpoint(resume,checkpoint,output_dir):
+    """
+    Resume the existing model checkpoints and set checkpoint directory
+    """
+    if resume:
+        if checkpoint:
+            raise ValueError('resume and checkpoint cannot both be specified')
+        checkpoint = output_dir
+   return checkpoint
+
+def set_seed(seed):
+    if args.seed is not None:
+        tf.set_random_seed(seed)
+        np.random.seed(seed)
+        random.seed(seed)
+
+def load_params_from_checkpoints_dir(checkpoint,dataset,model):
+    if checkpoint:
+        checkpoint_dir = os.path.normpath(checkpoint)
+        if not os.path.isdir(checkpoint):
+            checkpoint_dir, _ = os.path.split(checkpoint_dir)
+        if not os.path.exists(checkpoint_dir):
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), checkpoint_dir)
+        with open(os.path.join(checkpoint_dir, "options.json")) as f:
+            print("loading options from checkpoint %s" % args.checkpoint)
+            options = json.loads(f.read())
+            dataset = dataset or options['dataset']
+            model = model or options['model']
+        try:
+            with open(os.path.join(checkpoint_dir, "model_hparams.json")) as f:
+                model_hparams_dict.update(json.loads(f.read()))
+        except FileNotFoundError:
+            print("model_hparams.json was not loaded because it does not exist")
+    return dataset, model, checkpoint_dir
+
+def setup_dataset(dataset,input_dir,val_input_dir):
+    VideoDataset = datasets.get_dataset_class(dataset)
+    train_dataset = VideoDataset(
+        input_dir,
+        mode='train')
+    val_dataset = VideoDataset(
+        val_input_dir or input_dir,
+        mode='val')
+    variable_scope = tf.get_variable_scope()
+    variable_scope.set_use_resource(True)
+    return train_dataset,val_dataset,variable_scope
+
+def setup_model(model,model_hparams_dict,train_dataset,model_hparams):
+    """
+    Set up model instance
+    """
+    VideoPredictionModel = models.get_model_class(model)
+    hparams_dict = dict(model_hparams_dict)
+    hparams_dict.update({
+        'context_frames': train_dataset.hparams.context_frames,
+        'sequence_length': train_dataset.hparams.sequence_length,
+        'repeat': train_dataset.hparams.time_shift,
+    })
+    model = VideoPredictionModel(
+        hparams_dict=hparams_dict,
+        hparams=model_hparams)
+   return model
+
+def save_dataset_model_params_to_checkpoint_dir(args,output_dir,train_dataset,model):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    with open(os.path.join(output_dir, "options.json"), "w") as f:
+        f.write(json.dumps(vars(args), sort_keys=True, indent=4))
+    with open(os.path.join(output_dir, "dataset_hparams.json"), "w") as f:
+        f.write(json.dumps(train_dataset.hparams.values(), sort_keys=True, indent=4))
+    with open(os.path.join(args.output_dir, "model_hparams.json"), "w") as f:
+        f.write(json.dumps(model.hparams.values(), sort_keys=True, indent=4))
+   return None
+
+def make_dataset_iterator(train_dataset, val_dataset, batch_size ):
+    train_tf_dataset = train_dataset.make_dataset_v2(batch_size)
+    train_iterator = train_tf_dataset.make_one_shot_iterator()
+    # The `Iterator.string_handle()` method returns a tensor that can be evaluated
+    # and used to feed the `handle` placeholder.
+    train_handle = train_iterator.string_handle()
+    val_tf_dataset = val_dataset.make_dataset_v2(batch_size)
+    val_iterator = val_tf_dataset.make_one_shot_iterator()
+    val_handle = val_iterator.string_handle()
+    #iterator = tf.data.Iterator.from_string_handle(
+    #    train_handle, train_tf_dataset.output_types, train_tf_dataset.output_shapes)
+    inputs = train_iterator.get_next()
+    val = val_iterator.get_next()
+    return inputs, val_handle
+
+
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -40,116 +161,49 @@ def main():
 
     parser.add_argument("--dataset", type=str, help="dataset class name")
     parser.add_argument("--model", type=str, help="model class name")
-    parser.add_argument("--model_hparams", type=str, help="a string of comma separated list of model hyperparameters")
     parser.add_argument("--model_hparams_dict", type=str, help="a json file of model hyperparameters")
 
-   # parser.add_argument("--aggregate_nccl", type=int, default=0, help="whether to use nccl or cpu for gradient aggregation in multi-gpu training")
     parser.add_argument("--gpu_mem_frac", type=float, default=0, help="fraction of gpu memory to use")
-    parser.add_argument("--seed", type=int)
+    parser.add_argument("--seed",default=1234, type=int)
 
     args = parser.parse_args()
+     
+    #Set seed  
+    set_seed(args.seed)
+    
+    #setup output directory
+    args.output_dir = generate_output_dir(args.output_dir, args.model, args.model_hparams, args.logs_dir, args.output_dir_postfix)
+    
+    #resume the existing checkpoint and set up the checkpoint directory to output directory
+    args.checkpoint = resume_checkpoint(args.resume,args.checkpoint,args.output_dir)
+ 
+    #get model hparams dict from json file
+    model_hparams_dict = get_model_hparams_dict(args.model_hparams_dict)
+    
 
-    if args.seed is not None:
-        tf.set_random_seed(args.seed)
-        np.random.seed(args.seed)
-        random.seed(args.seed)
-
-    if args.output_dir is None:
-        list_depth = 0
-        model_fname = ''
-        for t in ('model=%s,%s' % (args.model, args.model_hparams)):
-            if t == '[':
-                list_depth += 1
-            if t == ']':
-                list_depth -= 1
-            if list_depth and t == ',':
-                t = '..'
-            if t in '=,':
-                t = '.'
-            if t in '[]':
-                t = ''
-            model_fname += t
-        args.output_dir = os.path.join(args.logs_dir, model_fname) + args.output_dir_postfix
-
-    if args.resume:
-        if args.checkpoint:
-            raise ValueError('resume and checkpoint cannot both be specified')
-        args.checkpoint = args.output_dir
-
-
-    model_hparams_dict = {}
-    if args.model_hparams_dict:
-        with open(args.model_hparams_dict) as f:
-            model_hparams_dict.update(json.loads(f.read()))
-    if args.checkpoint:
-        checkpoint_dir = os.path.normpath(args.checkpoint)
-        if not os.path.isdir(args.checkpoint):
-            checkpoint_dir, _ = os.path.split(checkpoint_dir)
-        if not os.path.exists(checkpoint_dir):
-            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), checkpoint_dir)
-        with open(os.path.join(checkpoint_dir, "options.json")) as f:
-            print("loading options from checkpoint %s" % args.checkpoint)
-            options = json.loads(f.read())
-            args.dataset = args.dataset or options['dataset']
-            args.model = args.model or options['model']
-        try:
-            with open(os.path.join(checkpoint_dir, "model_hparams.json")) as f:
-                model_hparams_dict.update(json.loads(f.read()))
-        except FileNotFoundError:
-            print("model_hparams.json was not loaded because it does not exist")
-
+    #load the existing checkpoint related datasets, model configure (This information was stored in the checkpoint dir when last time training model)
+    args.dataset,args.model,checkpoint_dir = load_params_from_checkpoints_dir(args.checkpoint,args.dataset,args.model)
+     
     print('----------------------------------- Options ------------------------------------')
     for k, v in args._get_kwargs():
         print(k, "=", v)
     print('------------------------------------- End --------------------------------------')
-
-    VideoDataset = datasets.get_dataset_class(args.dataset)
-    train_dataset = VideoDataset(
-        args.input_dir,
-        mode='train')
-    val_dataset = VideoDataset(
-        args.val_input_dir or args.input_dir,
-        mode='val')
-
-    variable_scope = tf.get_variable_scope()
-    variable_scope.set_use_resource(True)
-
-    VideoPredictionModel = models.get_model_class(args.model)
-    hparams_dict = dict(model_hparams_dict)
-    hparams_dict.update({
-        'context_frames': train_dataset.hparams.context_frames,
-        'sequence_length': train_dataset.hparams.sequence_length,
-        'repeat': train_dataset.hparams.time_shift,
-    })
-    model = VideoPredictionModel(
-        hparams_dict=hparams_dict,
-        hparams=args.model_hparams)
+    #setup training val datset instance
+    train_dataset,val_dataset,variable_scope = setup_dataset(dataset,args.input_dir,args.val_input_dir)
+    
+    #setup model instance 
+    model=setup_model(args.model,model_hparams_dict,train_dataset,args.model_hparams)
 
     batch_size = model.hparams.batch_size
-    train_tf_dataset = train_dataset.make_dataset_v2(batch_size)
-    train_iterator = train_tf_dataset.make_one_shot_iterator()
-    # The `Iterator.string_handle()` method returns a tensor that can be evaluated
-    # and used to feed the `handle` placeholder.
-    train_handle = train_iterator.string_handle()
-    val_tf_dataset = val_dataset.make_dataset_v2(batch_size)
-    val_iterator = val_tf_dataset.make_one_shot_iterator()
-    val_handle = val_iterator.string_handle()
-    #iterator = tf.data.Iterator.from_string_handle(
-    #    train_handle, train_tf_dataset.output_types, train_tf_dataset.output_shapes)
-    inputs = train_iterator.get_next()
-    val = val_iterator.get_next()
-   
+    #Create input and val iterator
+    inputs,val_handle = make_dataset_iterator(train_dataset, val_dataset, batch_size)
+    
+    #build model graph
     model.build_graph(inputs)
-
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
-    with open(os.path.join(args.output_dir, "options.json"), "w") as f:
-        f.write(json.dumps(vars(args), sort_keys=True, indent=4))
-    with open(os.path.join(args.output_dir, "dataset_hparams.json"), "w") as f:
-        f.write(json.dumps(train_dataset.hparams.values(), sort_keys=True, indent=4))
-    with open(os.path.join(args.output_dir, "model_hparams.json"), "w") as f:
-        f.write(json.dumps(model.hparams.values(), sort_keys=True, indent=4))
-
+    
+    #save all the model, data params to output dirctory
+    save_dataset_model_params_to_checkpoint_dir(args,args.output_dir,train_dataset,model)
+    
     with tf.name_scope("parameter_count"):
         # exclude trainable variables that are replicas (used in multi-gpu setting)
         trainable_variables = set(tf.trainable_variables()) & set(model.saveable_variables)
@@ -162,65 +216,38 @@ def main():
 
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_mem_frac, allow_growth=True)
     config = tf.ConfigProto(gpu_options=gpu_options, allow_soft_placement=True)
-  
  
-    max_steps = model.hparams.max_steps
-    print ("max_steps",max_steps)
+    max_steps = model.hparams.max_steps    
     with tf.Session(config=config) as sess:
         print("parameter_count =", sess.run(parameter_count))
         sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())
-        #coord = tf.train.Coordinator()
-        #threads = tf.train.start_queue_runners(sess = sess, coord = coord)
-        print("Init done: {sess.run(tf.local_variables_initializer())}%")
         model.restore(sess, args.checkpoint)
-
-        #sess.run(model.post_init_ops)
-
-        #val_handle_eval = sess.run(val_handle)
-        #print ("val_handle_val",val_handle_eval)
-        #print("val handle done")
         sess.graph.finalize()
         start_step = sess.run(model.global_step)
-
-
         # start at one step earlier to log everything without doing any training
         # step is relative to the start_step
         for step in range(-1, max_steps - start_step):
             global_step = sess.run(model.global_step)
             print ("global_step:", global_step)
             val_handle_eval = sess.run(val_handle)
-
-            if step == 1:
-                # skip step -1 and 0 for timing purposes (for warmstarting)
-                start_time = time.time()
             
+            #Fetch variables in the graph
             fetches = {"global_step":model.global_step}
             fetches["train_op"] = model.train_op
-
-           # fetches["latent_loss"] = model.latent_loss
+            #fetches["latent_loss"] = model.latent_loss
             fetches["total_loss"] = model.total_loss
+
+            #fetch the specific loss function only for mcnet
             if model.__class__.__name__ == "McNetVideoPredictionModel":
                 fetches["L_p"] = model.L_p
                 fetches["L_gdl"] = model.L_gdl
                 fetches["L_GAN"]  =model.L_GAN
-          
-         
-
-            fetches["summary"] = model.summary_op
-
-            run_start_time = time.time()
-            #Run training results
-            #X = inputs["images"].eval(session=sess)           
-
+            
+            fetches["summary"] = model.summary_op       
             results = sess.run(fetches)
-
-            run_elapsed_time = time.time() - run_start_time
-            if run_elapsed_time > 1.5 and step > 0 and set(fetches.keys()) == {"global_step", "train_op"}:
-                print('running train_op took too long (%0.1fs)' % run_elapsed_time)
-
-            #Run testing results
-            #val_fetches = {"global_step":global_step}
+            
+            #Fetch losses for validation data
             val_fetches = {}
             #val_fetches["latent_loss"] = model.latent_loss
             #val_fetches["total_loss"] = model.total_loss
@@ -229,45 +256,23 @@ def main():
           
             summary_writer.add_summary(results["summary"])
             summary_writer.add_summary(val_results["summary"])
-             
-
-
-           
-            val_datasets = [val_dataset]
-            val_models = [model]
-
-            # for i, (val_dataset_, val_model) in enumerate(zip(val_datasets, val_models)):
-            #     sess.run(val_model.accum_eval_metrics_reset_op)
-            #     # traverse (roughly up to rounding based on the batch size) all the validation dataset
-            #     accum_eval_summary_num_updates = val_dataset_.num_examples_per_epoch() // val_model.hparams.batch_size
-            #     val_fetches = {"global_step": global_step, "accum_eval_summary": val_model.accum_eval_summary_op}
-            #     for update_step in range(accum_eval_summary_num_updates):
-            #         print('evaluating %d / %d' % (update_step + 1, accum_eval_summary_num_updates))
-            #         val_results = sess.run(val_fetches, feed_dict={train_handle: val_handle_eval})
-            #     accum_eval_summary = add_tag_suffix(val_results["accum_eval_summary"], '_%d' % (i + 1))
-            #     print("recording accum eval summary")
-            #     summary_writer.add_summary(accum_eval_summary, val_results["global_step"])
             summary_writer.flush()
-
+             
             # global_step will have the correct step count if we resume from a checkpoint
             # global step is read before it's incremented
             steps_per_epoch = train_dataset.num_examples_per_epoch() / batch_size
-            #train_epoch = results["global_step"] / steps_per_epoch
             train_epoch = global_step/steps_per_epoch
             print("progress  global step %d  epoch %0.1f" % (global_step + 1, train_epoch))
-            if step > 0:
-                elapsed_time = time.time() - start_time
-                average_time = elapsed_time / step
-                images_per_sec = batch_size / average_time
-                remaining_time = (max_steps - (start_step + step + 1)) * average_time
-                print("image/sec %0.1f  remaining %dm (%0.1fh) (%0.1fd)" %
-                      (images_per_sec, remaining_time / 60, remaining_time / 60 / 60, remaining_time / 60 / 60 / 24))
 
-
-            print("Total_loss:{}; L_p_loss:{}; L_gdl:{}; L_GAN: {}".format(results["total_loss"],results["L_p"],results["L_gdl"],results["L_GAN"]))
+            if model.__class__.__name__ == "McNetVideoPredictionModel":
+              print("Total_loss:{}; L_p_loss:{}; L_gdl:{}; L_GAN: {}".format(results["total_loss"],results["L_p"],results["L_gdl"],results["L_GAN"]))
+            elif model.__class__.__name__ == "VanillaConvLstmVideoPredictionModel":
+                print ("Total_loss:{}".format(results["total_loss"]))
+            else:
+                print ("The model name does not exist")
             
             print("saving model to", args.output_dir)
-            saver.save(sess, os.path.join(args.output_dir, "model"), global_step=step)##Bing: cheat here a little bit because of the global step issue
+            saver.save(sess, os.path.join(args.output_dir, "model"), global_step=step)#
             print("done")
 
 if __name__ == '__main__':
