c7a0ae55e37ca42c0465295733597196cab30e6d
Michael <m.langguth@fz-juelich.de>
2020-07-01 12:47:57 +0200

    Initial release of specific modules.sh-versions to be used during Preprocessing and training.

--- video_prediction_savp/env_setup/modules_train.sh
+++ video_prediction_savp/env_setup/modules_train.sh
@@ -14,24 +14,11 @@ module purge
 module use $OTHERSTAGES
 module load Stages/2019a
 module load GCC/8.3.0
-# MVAPICH2 is currently not needed at all?
-#if [[ "${HOST_NAME}" == hdfml* ]]; then
-  # MVAPICH2 conflicts with h5py (since h5py requires MPIParaStation)
-  #module load MVAPICH2/2.3.3-GDR
-#if [[ "${HOST_NAME}" == juwels* ]]; then
-#  module load MVAPICH2/.2.3.1-GDR
-#fi
 module load GCCcore/.8.3.0
 module load ParaStationMPI/5.2.2-1
 module load mpi4py/3.0.1-Python-3.6.8
-# serialized version is not available on HFML
-# see https://gitlab.version.fz-juelich.de/haf/Wiki/-/wikis/HDF-ML%20System
-if [[ "${HOST_NAME}" == hdfml* ]]; then
-  module load h5py/2.9.0-serial-Python-3.6.8
-elif [[ "${HOST_NAME}" == juwels* ]]; then
-  module load h5py/2.9.0-Python-3.6.8
-fi
-module load netcdf4-python/1.5.0.1-Python-3.6.8
+# serialized version of HDF5 is used since only this version is compatible with TensorFlow/1.13.1-GPU-Python-3.6.8
+module load h5py/2.9.0-serial-Python-3.6.8
 module load TensorFlow/1.13.1-GPU-Python-3.6.8
 module load cuDNN/7.5.1.10-CUDA-10.1.105
 
