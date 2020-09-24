84a5db9c38d54a6d7aacd6de096448d62e7c3f12
Michael <m.langguth@fz-juelich.de>
2020-09-24 14:10:35 +0200

    Adaption of destination_dir according to updated convention for storing trained model (data), i.e. add user and timestamp.

--- video_prediction_savp/HPC_scripts/train_era5_template.sh
+++ video_prediction_savp/HPC_scripts/train_era5_template.sh
@@ -34,18 +34,16 @@ if [ -z ${VIRTUAL_ENV} ]; then
    fi
 fi
 
-
-
-
 # declare directory-variables which will be modified appropriately during Preprocessing (invoked by mpi_split_data_multi_years.py)
 source_dir=/p/project/deepacf/deeprain/video_prediction_shared_folder/preprocessedData/
 destination_dir=/p/project/deepacf/deeprain/video_prediction_shared_folder/models/
 
-# for choosing the model for choosing the model, convLSTM,savp, mcnet,vae
+# valid identifiers for model-argument are: convLSTM, savp, mcnet and vae
 model=convLSTM
 model_hparams=../hparams/era5/${model}/model_hparams.json
+destination_dir=${destination_dir}/${model}/"$(date +"%Y%m%dT%H%M")_"$USER"/"
 
-# rund training
-srun python ../scripts/train_dummy.py --input_dir  ${source_dir}/tfrecords/ --dataset era5  --model ${model} --model_hparams_dict ${model_hparams} --output_dir ${destination_dir}/${model}/
+# run training
+srun python ../scripts/train_dummy.py --input_dir  ${source_dir}/tfrecords/ --dataset era5  --model ${model} --model_hparams_dict ${model_hparams} --output_dir ${destination_dir}
 
  
