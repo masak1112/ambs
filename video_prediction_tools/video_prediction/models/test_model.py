94b55778b77ee5fd7067abf1f84f264390f3c14d
gong1 <b.gong@fz-juelich.de>
2020-11-10 11:38:11 +0100

    add a test_model class, which is a simple linear regression model with 2 variables. This is only used for unitest

---
+++ video_prediction_tools/video_prediction/models/test_model.py
@@ -0,0 +1,108 @@
+
+
+__email__ = "b.gong@fz-juelich.de" 
+__author__ = "Bing Gong, Scarlet Stadtler,Michael Langguth" 
+__date__ = "2020-11-05" 
+
+import collections
+import functools
+import itertools
+from collections import OrderedDict
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.util import nest
+from video_prediction import ops, flow_ops
+from video_prediction.models import BaseVideoPredictionModel
+from video_prediction.models import networks
+from video_prediction.ops import dense, pad2d, conv2d, flatten, tile_concat
+from video_prediction.rnn_ops import BasicConv2DLSTMCell, Conv2DGRUCell
+from video_prediction.utils import tf_utils
+from datetime import datetime
+from pathlib import Path
+from video_prediction.layers import layer_def as ld
+from video_prediction.layers.BasicConvLSTMCell import BasicConvLSTMCell
+from tensorflow.contrib.training import HParams
+
+class TestModelVideoPredictionModel(object):
+    def __init__(self, mode='train', hparams_dict=None):
+        """
+        This is class for building convLSTM architecture by using updated hparameters
+        args:
+             mode   :str, "train" or "val", side note: mode may not be used in the convLSTM, but this will be a useful argument for the GAN-based model
+             hparams_dict: dict, the dictionary contains the hparaemters names and values
+        """
+        self.mode = mode
+        self.hparams_dict = hparams_dict
+        self.hparams = self.parse_hparams()        
+        self.learning_rate = self.hparams.lr
+        self.total_loss = None
+        self.context_frames = self.hparams.context_frames
+        self.sequence_length = self.hparams.sequence_length
+        self.predict_frames = self.sequence_length - self.context_frames
+        self.max_epochs = self.hparams.max_epochs
+        self.loss_fun = self.hparams.loss_fun
+
+
+    def get_default_hparams(self):
+        return HParams(**self.get_default_hparams_dict())
+
+    def parse_hparams(self):
+        """
+        Parse the hparams setting to ovoerride the default ones
+        """
+        
+        parsed_hparams = self.get_default_hparams().override_from_dict(self.hparams_dict or {})
+        return parsed_hparams
+
+
+    def get_default_hparams_dict(self):
+        """
+        The function that contains default hparams
+        Returns:
+            A dict with the following hyperparameters.
+            context_frames  : the number of ground-truth frames to pass in at start.
+            sequence_length : the number of frames in the video sequence 
+            max_epochs      : the number of epochs to train model
+            lr              : learning rate
+            loss_fun        : the loss function
+        """
+        hparams = dict(
+            context_frames=10,
+            sequence_length=20,
+            max_epochs = 20,
+            batch_size = 40,
+            lr = 0.001,
+            loss_fun = "cross_entropy",
+            shuffle_on_val= True,
+        )
+        return hparams
+
+
+    def build_graph(self, x):
+        self.is_build_graph = False
+        self.x = x["images"]
+        self.global_step = tf.train.get_or_create_global_step()
+        original_global_variables = tf.global_variables()
+        # ARCHITECTURE
+        self.test_model_network()
+        self.total_loss = tf.reduce_mean(tf.square(self.y_data - self.y))
+        self.train_op = tf.train.AdamOptimizer(
+            learning_rate = self.learning_rate).minimize(self.total_loss, global_step = self.global_step)
+        # Summary op
+        self.loss_summary = tf.summary.scalar("total_loss", self.total_loss)
+        self.summary_op = tf.summary.merge_all()
+        global_variables = [var for var in tf.global_variables() if var not in original_global_variables]
+        self.saveable_variables = [self.global_step] + global_variables
+        self.is_build_graph = True
+        return self.is_build_graph 
+
+
+    def test_model_network(self):
+        """
+        Build a simple linear model, this is just for testing
+        """
+        x_data = np.random.rand(100).astype(np.float)
+        self.y_data = x_data * 0.1 + 0.3
+        weights = tf.Variable(tf.random_uniform([1],-1,1.0),name="weights")
+        biases = tf.Variable(tf.zeros([1]),name="biases")
+        self.y = x_data * weights + biases
