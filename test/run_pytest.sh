3113f9a893c0704fc80d340581e4d5427f6021e7
gong1 <b.gong@fz-juelich.de>
2020-11-10 14:08:47 +0100

    update run_pytest

--- test/run_pytest.sh
+++ test/run_pytest.sh
@@ -22,10 +22,10 @@ fi
 source ../video_prediction_tools/env_setup/modules_train.sh
 #Test for process step2
 #python -m pytest test_data_preprocess_step2.py
-python -m pytest test_era5_data.py
+#python -m pytest test_era5_data.py
 #Test for training
 #First remove all the files in the test folder
-#rm /p/project/deepacf/deeprain/video_prediction_shared_folder/models/test/* 
-#python -m pytest test_train_model_era5.py
+rm /p/project/deepacf/deeprain/video_prediction_shared_folder/models/test/* 
+python -m pytest test_train_model_era5.py
 
 
