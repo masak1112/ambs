5faeb90d08615b375628306059a305cfd0bcf1c9
Michael <m.langguth@fz-juelich.de>
2020-11-16 11:50:36 +0100

    A couple of source code style changes, typo removal etc. pp.

--- video_prediction_tools/data_preprocess/preprocess_data_step2.py
+++ video_prediction_tools/data_preprocess/preprocess_data_step2.py
@@ -1,53 +1,43 @@
-
+"""
+Class and functions required for preprocessing ERA5 data (preprocessing substep 2)
+"""
 __email__ = "b.gong@fz-juelich.de"
-__author__ = "Bing Gong, Scarlet Stadtler,Michael Langguth"
+__author__ = "Bing Gong, Scarlet Stadtler, Michael Langguth"
 __date__ = "2020_11_10"
 
-
-import argparse
+# import modules
 import os
-import glob
-import sys
-import itertools
 import pickle
-import random
-import re
 import numpy as np
 import json
 import tensorflow as tf
-from collections import OrderedDict
-from tensorflow.contrib.training import HParams
-from mpi4py import MPI
-from video_prediction.datasets.base_dataset import VarLenFeatureVideoDataset
-import data_preprocess.process_netCDF_v2
-from general_utils import get_unique_vars
-from statistics import Calc_data_stat
-from metadata import MetaData
 from normalization import Norm_data
-from google.protobuf.json_format import MessageToDict
 import datetime
 from video_prediction.datasets import ERA5Dataset
 
 
 class ERA5Pkl2Tfrecords(ERA5Dataset):
-    def __init__(self,input_dir=None, output_dir=None,datasplit_config=None,hparams_dict_config=None,sequences_per_file=128,norm="minmax"):
+    def __init__(self, input_dir=None, output_dir=None, datasplit_config=None, hparams_dict_config=None, \
+                 sequences_per_file=128, norm="minmax"):
         """
         This class is used for converting pkl files to tfrecords
         args:
-            input_dir            : str, the path to the PreprocessData directory which is parent directory of "Pickle" and "tfrecords" files directiory. 
+            input_dir            : str, the path to the PreprocessData directory which is parent directory of "Pickle"
+                                   and "tfrecords" files directiory.
             outpout_dir          : str, the one upper  level of the path to save the tfrecords files 
             datasplit_config     : the path pointing to the datasplit_config json file
             hparams_dict_config  : the path to the dict that contains hparameters,
             sequences_per_file   : int, how many sequences/samples per tfrecord to be saved
-            norm                 : str, normalization methods from Norm_data class, "minmax" or "znorm" default is "minmax", 
+            norm                 : str, normalization methods from Norm_data class ("minmax" or "znorm";
+                                   default: "minmax")
         """
         self.input_dir = input_dir
-        self.output_dir = os.path.join(output_dir,"tfrecords")
-        #if the output_dir is not exist, then create it
-        os.makedirs(self.output_dir,exist_ok=True)
-        #get metadata,includes the var_in, image height, width etc.
+        self.output_dir = os.path.join(output_dir, "tfrecords")
+        # if the output_dir is not exist, then create it
+        os.makedirs(self.output_dir, exist_ok=True)
+        # get metadata,includes the var_in, image height, width etc.
         self.get_metadata()
-        #Get the data split informaiton
+        # Get the data split informaiton
         self.datasplit_dict_path = datasplit_config
         self.data_dict = self.get_datasplit()
         self.hparams_dict_config = hparams_dict_config      
@@ -57,10 +47,9 @@ class ERA5Pkl2Tfrecords(ERA5Dataset):
         if norm == "minmax" or norm == "znorm":
             self.norm = norm
         else:
-            raise ("norm should be either 'minmax' or 'znorm'") 
+            raise ValueError("norm should be either 'minmax' or 'znorm'")
         self.sequences_per_file = sequences_per_file
         self.write_sequence_file()
-   
 
     def get_years_months(self):
         """
@@ -76,15 +65,15 @@ class ERA5Pkl2Tfrecords(ERA5Dataset):
             for year, month in value.items():
                 self.years.append(year)
                 self.months.extend(month)
-        return set(self.years),set(self.months)
+        return set(self.years), set(self.months)
 
     def get_stats_file(self):
         """
-        Get the correspoding statistic file
+        Get the corresponding statistics file
         """
-        pkl_dir = os.path.join(self.input_dir,"pickle")
-        print("Opening json-file: " + os.path.join(pkl_dir,"statistics.json"))
-        self.stats_file = os.path.join(pkl_dir,"statistics.json")
+        pkl_dir = os.path.join(self.input_dir, "pickle")
+        print("Opening json-file: " + os.path.join(pkl_dir, "statistics.json"))
+        self.stats_file = os.path.join(pkl_dir, "statistics.json")
         if os.path.isfile(self.stats_file):
             with open(self.stats_file) as js_file:
                 self.stats = json.load(js_file)
@@ -93,12 +82,13 @@ class ERA5Pkl2Tfrecords(ERA5Dataset):
 
     def get_metadata(self):
         """
-        This function gets the meta data that generared from data_process_step1, we aim to extract the height and width informaion from it
+        This function gets the meta data that has been generated in data_process_step1. Here, we aim to extract
+        the height and width information from it
         vars_in   : list(str), must be consistent with the list from DataPreprocessing_step1
         height    : int, the height of the image
         width     : int, the width of the image
         """
-        metadata_fl = os.path.join(self.input_dir,"metadata.json")
+        metadata_fl = os.path.join(self.input_dir, "metadata.json")
         if os.path.isfile(metadata_fl):
             self.metadata_fl = metadata_fl
             with open(self.metadata_fl) as f:
@@ -110,20 +100,22 @@ class ERA5Pkl2Tfrecords(ERA5Dataset):
             self.vars_in = [list(var.values())[0] for var in self.variables]
            
         else:
-            raise ("The metadata_file is not generated properly, you might need to re-run previous step of the workflow")
-
+            raise FileNotFoundError("The metadata_file is not generated properly, " + \
+                                    "you might need to re-run previous step of the workflow")
 
     @staticmethod
     def save_tf_record(output_fname, sequences, t_start_points):
         """
-        Save the squences, and the corresdponding timestamp start point to tfrecords
+        Save the sequences, and the corresponding timestamp start point to tfrecords
         args:
             output_frames    : str, the file names of the output
-            sequences        : list or array, the sequences want to be saved to tfrecords, [sequences,seq_len,height,width,channels]
-            t_start_points   : datetime type in the list,  the first timestamp for each sequence [seq_len,height,width, channel], the len of t_start_points is the same as sequences
+            sequences        : list or array, the sequences want to be saved to tfrecords,
+                               [sequences,seq_len,height,width,channels]
+            t_start_points   : datetime type in the list, the first timestamp for each sequence
+                               [seq_len,height,width, channel], the len of t_start_points is the same as sequences
         """
         sequences = np.array(sequences)
-
+        # sanity checks
         assert sequences.shape[0] == len(t_start_points)
         assert type(t_start_points[0]) == datetime.datetime
 
@@ -146,8 +138,6 @@ class ERA5Pkl2Tfrecords(ERA5Dataset):
                 example = tf.train.Example(features=features)
                 writer.write(example.SerializeToString())
 
-
-
     def initia_norm_class(self):
         """
         Get normalization data class 
@@ -156,60 +146,60 @@ class ERA5Pkl2Tfrecords(ERA5Dataset):
         # init normalization-instance
         self.norm_cls = Norm_data(self.vars_in)
         self.nvars = len(self.vars_in)
-        #get statistic file
+        # get statistics file
         self.get_stats_file()
         # open statistics file and feed it to norm-instance
-        self.norm_cls.check_and_set_norm(self.stats,self.norm)
+        self.norm_cls.check_and_set_norm(self.stats, self.norm)
 
-
-    def normalize_vars_per_seq(self,sequences):
+    def normalize_vars_per_seq(self, sequences):
         """
         Normalize all the variables for the sequences
         args:
-            sequences: list or array, is the sequences need to be saved to tfrecorcd. the shape should be [sequences_per_file,seq_length,height,width,nvars]
+            sequences: list or array, is the sequences need to be saved to tfrecorcd.
+                       The shape should be [sequences_per_file,seq_length,height,width,nvars]
         Return:
             the normalized sequences
         """
         assert len(np.array(sequences).shape) == 5
-        #Normalization should adpot the selected variables, here we used duplicated channel temperature variables
+        # normalization should adpot the selected variables, here we used duplicated channel temperature variables
         sequences = np.array(sequences)
-        #normalization
+        # normalization
         for i in range(self.nvars):
-            sequences[:,:,:,:,i] = self.norm_cls.norm_var(sequences[:,:,:,:,i],self.vars_in[i],self.norm)
+            sequences[:,:,:,:,i] = self.norm_cls.norm_var(sequences[:,:,:,:,i], self.vars_in[i], self.norm)
         return sequences
 
     
-    def read_pkl_and_save_tfrecords(self,year,month):
+    def read_pkl_and_save_tfrecords(self, year, month):
         """
         Read pickle files based on month, to process and save to tfrecords,
         args:
             year    : int, the target year to save to tfrecord
             month   : int, the target month to save to tfrecord 
         """
-        #Define the input_file based on the year and month
-        self.input_file_year = os.path.join(self.input_dir,"pickle",str(year))
-        input_file = os.path.join(self.input_file_year,'X_{:02d}.pkl'.format(month))
-        temp_input_file = os.path.join(self.input_file_year,'T_{:02d}.pkl'.format(month))
+        # Define the input_file based on the year and month
+        self.input_file_year = os.path.join(self.input_dir, "pickle", str(year))
+        input_file = os.path.join(self.input_file_year, 'X_{:02d}.pkl'.format(month))
+        temp_input_file = os.path.join(self.input_file_year, 'T_{:02d}.pkl'.format(month))
 
         self.initia_norm_class()
         sequences = []
         t_start_points = []
         sequence_iter = 0
 
-        #try:
+        # try:
         with open(input_file, "rb") as data_file:
             X_train = pickle.load(data_file)
-        with open(temp_input_file,"rb") as temp_file:
-             T_train = pickle.load(temp_file)
+        with open(temp_input_file, "rb") as temp_file:
+            T_train = pickle.load(temp_file)
 
-        #check to make sure the X_train and T_train has the same length 
+        # check to make sure that X_train and T_train have the same length
         assert (len(X_train) == len(T_train))
 
         X_possible_starts = [i for i in range(len(X_train) - self.sequence_length)]
         for X_start in X_possible_starts:
             X_end = X_start + self.sequence_length
             seq = X_train[X_start:X_end,:,:,:]
-            #Recored the start point of the timestamps
+            # recording the start point of the timestamps
             t_start = T_train[X_start]  
             seq = list(np.array(seq).reshape((self.sequence_length, self.height, self.width, self.nvars)))
             if not sequences:
@@ -219,46 +209,40 @@ class ERA5Pkl2Tfrecords(ERA5Dataset):
             sequence_iter += 1
 
             if len(sequences) == self.sequences_per_file:
-                #Nomalize variables in the sequence
-                sequences = ERA5Pkl2Tfrecords.normalize_vars_per_seq(self,sequences)
-                output_fname = 'sequence_Y_{}_M_{}_{}_to_{}.tfrecords'.format(year,month,last_start_sequence_iter,sequence_iter - 1)
+                # normalize variables in the sequences
+                sequences = ERA5Pkl2Tfrecords.normalize_vars_per_seq(self, sequences)
+                output_fname = 'sequence_Y_{}_M_{}_{}_to_{}.tfrecords'.format(year, month, last_start_sequence_iter, \
+                                                                              sequence_iter - 1)
                 output_fname = os.path.join(self.output_dir, output_fname)
-                #Write to tfrecord
-                ERA5Pkl2Tfrecords.write_seq_to_tfrecord(output_fname,sequences,t_start_points)
+                # write to tfrecord
+                ERA5Pkl2Tfrecords.write_seq_to_tfrecord(output_fname, sequences, t_start_points)
                 t_start_points = []
                 sequences = []
-            print("Finished for input file",input_file)
+            print("Finished for input file", input_file)
 
 #         except FileNotFoundError as fnf_error:
 #             print(fnf_error)
 
-
     @staticmethod
-    def write_seq_to_tfrecord(output_fname,sequences,t_start_points):
+    def write_seq_to_tfrecord(output_fname, sequences, t_start_points):
         """
-        Function to check if the sequences has been processed. if yes, will skip it, otherwise save the sequences to output file
+        Function to check if the sequences has been processed.
+        If yes, the sequences are skipped, otherwise the sequences are saved to the output file
         """
         if os.path.isfile(output_fname):
             print(output_fname, 'already exists, skip it')
         else:
-            ERA5Pkl2Tfrecords.save_tf_record(output_fname, list(sequences), t_start_points)   
-
-
-
+            ERA5Pkl2Tfrecords.save_tf_record(output_fname, list(sequences), t_start_points)
 
     def write_sequence_file(self):
-        sequence_lengths_file = open(os.path.join(self.output_dir, 'sequence_lengths.txt'), 'w')
-        sequence_lengths_file.write("%d\n" % self.sequences_per_file)
-        sequence_lengths_file.close()
-            
+        with open(os.path.join(self.output_dir, 'sequence_lengths.txt'), 'w') as seq_file:
+            seq_file.write("%d\n" % self.sequences_per_file)
 
 #     def num_examples_per_epoch(self):
 #         with open(os.path.join(self.input_dir, 'sequence_lengths.txt'), 'r') as sequence_lengths_file:
 #             sequence_lengths = sequence_lengths_file.readlines()
 #         sequence_lengths = [int(sequence_length.strip()) for sequence_length in sequence_lengths]
 #         return np.sum(np.array(sequence_lengths) >= self.hparams.sequence_length)
-
-
 
 
 def _bytes_feature(value):
@@ -268,8 +252,10 @@ def _bytes_feature(value):
 def _bytes_list_feature(values):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=values))
 
+
 def _floats_feature(value):
     return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))   
--- video_prediction_tools/main_scripts/main_preprocess_data_step2.py
+++ video_prediction_tools/main_scripts/main_preprocess_data_step2.py
@@ -1,40 +1,30 @@
-
+"""
+Driver for preprocessing step 2 which parses the input arguments from the runscript
+and performs parallelization with OpenMPI.
+"""
 __email__ = "b.gong@fz-juelich.de"
 __author__ = "Bing Gong, Scarlet Stadtler,Michael Langguth"
 
-import argparse
-import sys
+# import modules
 import os
-import glob
-import itertools
-import pickle
-import random
-import re
-import numpy as np
-import json
-import tensorflow as tf
-from collections import OrderedDict
-from tensorflow.contrib.training import HParams
+import argparse
 from mpi4py import MPI
-from video_prediction.datasets.base_dataset import VarLenFeatureVideoDataset
-import data_preprocess.process_netCDF_v2
 from general_utils import get_unique_vars
 from statistics import Calc_data_stat
-from metadata import MetaData
-from normalization import Norm_data
 from video_prediction.datasets.era5_dataset import *
-
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-input_dir", type=str)
     parser.add_argument("-output_dir", type=str)
-    parser.add_argument("-datasplit_config",type=str,help="The path to the datasplit_config json file which contains the details of train/val/testing")
-    parser.add_argument("-hparams_dict_config", type=str, help="The path to the dict that contains hparameters.",default="")
-    parser.add_argument("-sequences_per_file",type=int,default=20)
+    parser.add_argument("-datasplit_config", type=str, \
+                        help="The path to the datasplit_config json file which contains the details of train/val/testing")
+    parser.add_argument("-hparams_dict_config", type=str,\
+                        help="The path to the dict that contains hparameters.", default="")
+    parser.add_argument("-sequences_per_file", type=int, default=20)
     args = parser.parse_args()
-    ins = ERA5Pkl2Tfrecords(input_dir=args.input_dir,output_dir=args.output_dir,
+    ins = ERA5Pkl2Tfrecords(input_dir=args.input_dir, output_dir=args.output_dir,
                             datasplit_config=args.datasplit_config,
                             hparams_dict_config=args.hparams_dict_config,
                             sequences_per_file=args.sequences_per_file)
@@ -42,7 +32,7 @@ def main():
     partition = ins.data_dict
     partition_data = partition.values()
     years, months = ins.get_years_months()
-    input_dir_pkl = os.path.join(args.input_dir,"pickle")
+    input_dir_pkl = os.path.join(args.input_dir, "pickle")
     # ini. MPI
     comm = MPI.COMM_WORLD
     my_rank = comm.Get_rank()  # rank of the node
@@ -51,7 +41,7 @@ def main():
     if my_rank == 0:
         # retrieve final statistics first (not parallelized!)
         # some preparatory steps
-        stat_dir_prefix =  input_dir_pkl
+        stat_dir_prefix = input_dir_pkl
         varnames        = ins.vars_in
     
         vars_uni, varsind, nvars = get_unique_vars(varnames)
@@ -62,13 +52,14 @@ def main():
         for split in partition.keys():
             values = partition[split]
             for year in values.keys():
-                file_dir = os.path.join(stat_dir_prefix,year)
+                file_dir = os.path.join(stat_dir_prefix, year)
                 for month in values[year]:
-                    if os.path.isfile(os.path.join(file_dir,"stat_" + '{0:02}'.format(month) + ".json")):
+                    if os.path.isfile(os.path.join(file_dir, "stat_" + '{0:02}'.format(month) + ".json")):
                         # process stat-file:
-                        stat_obj.acc_stat_master(file_dir,int(month))  # process monthly statistic-file  
+                        stat_obj.acc_stat_master(file_dir, int(month))  # process monthly statistic-file
                     else:
-                        raise ("The stat file does not exist:",os.path.join(file_dir,"stat_" + '{0:02}'.format(month) + ".json") )
+                        raise ("The stat file does not exist:", os.path.join(file_dir, "stat_" + '{0:02}'.\
+                                                                             format(month) + ".json"))
         # finalize statistics and write to json-file
         stat_obj.finalize_stat_master(vars_uni)
         stat_obj.write_stat_json(input_dir_pkl)
@@ -77,58 +68,52 @@ def main():
         
         real_years_months = []
         for year_months in partition_data:
-            print("I am here year:",year_months)
+            print("I am here year:", year_months)
             for year in year_months.keys():
                 for month in year_months[year]:
-                     print("I am here month",month)
-                     year_month = "Y_{}_M_{}".format(year,month)
-                     real_years_months.append(year_month)
+                    print("I am here month", month)
+                    year_month = "Y_{}_M_{}".format(year, month)
+                    real_years_months.append(year_month)
  
-        broadcast_lists = [list(years),real_years_months]
+        broadcast_lists = [list(years), real_years_months]
 
-        for nodes in range(1,p):
-            comm.send(broadcast_lists,dest=nodes) 
+        for nodes in range(1, p):
+            comm.send(broadcast_lists, dest=nodes)
            
         message_counter = 1
         while message_counter <= 12:
             message_in = comm.recv()
             message_counter = message_counter + 1 
-            print("Message in from slaver",message_in) 
-            
-        #write_sequence_file(args.output_dir,args.seq_length,args.sequences_per_file)
+            print("Message in from slave: ", message_in)
  
     else:
         message_in = comm.recv()
-        print ("My rank,", my_rank)   
-        print("message_in",message_in)
+        print("My rank,", my_rank)
+        print("message_in", message_in)
         
         years = list(message_in[0])
         real_years_months = message_in[1] 
    
         for year in years:
-            #check if the year_rank is in the datasplit_dict which we want to process, we will skip the month that not in the datasplit_dict
-            year_rank = "Y_{}_M_{}".format(year,my_rank)
+            # loop over the years in the datasplit_dict which we want to process,
+            # while months that are not in the datasplit_dict are skipped
+            year_rank = "Y_{}_M_{}".format(year, my_rank)
             if year_rank in real_years_months:
-                input_file = "X_" + '{0:02}'.format(my_rank) + ".pkl"
-                temp_file = "T_" + '{0:02}'.format(my_rank) + ".pkl"
-                input_dir = os.path.join(args.input_dir,year)
-                temp_file = os.path.join(input_dir,temp_file)
-                input_file = os.path.join(input_dir,input_file)
-                #Initilial instance
-                ins2 = ERA5Pkl2Tfrecords(input_dir=args.input_dir,output_dir=args.output_dir,
+                # Initilial instance
+                ins2 = ERA5Pkl2Tfrecords(input_dir=args.input_dir, output_dir=args.output_dir,
                             datasplit_config=args.datasplit_config,
-                            hparams_dict_config=args.hparams_dict_config,sequences_per_file=args.sequences_per_file)
+                            hparams_dict_config=args.hparams_dict_config, sequences_per_file=args.sequences_per_file)
                 # create the tfrecords-files
-                ins2.read_pkl_and_save_tfrecords(year=year,month=my_rank)
-                print("Year {} finished",year)
+                ins2.read_pkl_and_save_tfrecords(year=year, month=my_rank)
+                print("Year {} finished", year)
             else:
-                print (year_rank + " is not in the datasplit_dic, will skip the process")
-        message_out = ("Node:",str(my_rank),"finished","","\r\n")
-        print ("Message out for slaves:",message_out)
-        comm.send(message_out,dest=0)
+                print(year_rank + " is not in the datasplit_dic, will skip the process")
+        message_out = ("Node:", str(my_rank), "finished", "", "\r\n")
+        print("Message out for slaves:", message_out)
+        comm.send(message_out, dest=0)
 
-    MPI.Finalize()        
+    MPI.Finalize()
+
    
 if __name__ == '__main__':
-     main()
-
+    main()
