9a5b4c7f9b93bc6fc779a2e53f3b3c1381cba2c7
Michael <m.langguth@fz-juelich.de>
2020-11-19 13:14:10 +0100

    Adaption in hostname-detection for setting e-mail address to runscripts.

--- video_prediction_tools/env_setup/generate_workflow_runscripts.sh
+++ video_prediction_tools/env_setup/generate_workflow_runscripts.sh
@@ -156,7 +156,7 @@ if [[ `grep "exp_id=" ${target_script}` ]]; then
 fi
 
 # set correct e-mail address in Batch scripts on Juwels and HDF-ML
-if [[ "${HOST_NAME}" == hdfml* || "${HOST_NAME}" == juwels* ]]; then
+if [[ "${HOST_NAME}" == hdfml* || "${HOST_NAME}" == *juwels* ]]; then
   if ! [[ -z `command -v jutil` ]]; then
     USER_EMAIL=$(jutil user show -o json | grep email | cut -f2 -d':' | cut -f1 -d',' | cut -f2 -d'"')
   else
