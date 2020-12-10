e0246ada66c4f96741c96132df9da0796658db24
stadtler1 <s.stadtler@fz-juelich.de>
2020-12-10 13:59:47 +0100

    Minor changes to make the environment creation run on Juwels. Additionally lower features so it can run on Juwels too (not only on the booster). Some conflicts has been solved to avoid unwanted inference from scarlet_issue#031_booster.

--- video_prediction_tools/env_setup/create_env.sh
+++ video_prediction_tools/env_setup/create_env.sh
@@ -103,7 +103,6 @@ if [[ "$ENV_EXIST" == 0 ]]; then
   # expand PYTHONPATH...
   export PYTHONPATH=${WORKING_DIR}:$PYTHONPATH >> ${activate_virt_env}
   export PYTHONPATH=${WORKING_DIR}/utils:$PYTHONPATH >> ${activate_virt_env}
-  #export PYTHONPATH=/p/home/jusers/${USER}/juwels/.local/bin:$PYTHONPATH
   export PYTHONPATH=${WORKING_DIR}/external_package/lpips-tensorflow:$PYTHONPATH >> ${activate_virt_env}
   export PYTHONPATH=${WORKING_DIR}/model_modules:$PYTHONPATH >> ${activate_virt_env}
 
@@ -116,7 +115,6 @@ if [[ "$ENV_EXIST" == 0 ]]; then
   echo "export PYTHONPATH=${WORKING_DIR}:\$PYTHONPATH" >> ${activate_virt_env}
   echo "export PYTHONPATH=${WORKING_DIR}/utils/:\$PYTHONPATH" >> ${activate_virt_env}
   echo "export PYTHONPATH=${WORKING_DIR}/model_modules:$PYTHONPATH " >> ${activate_virt_env}
-  #export PYTHONPATH=/p/home/jusers/${USER}/juwels/.local/bin:\$PYTHONPATH
   echo "export PYTHONPATH=${WORKING_DIR}/external_package/lpips-tensorflow:\$PYTHONPATH" >> ${activate_virt_env}
 
   if [[ "${HOST_NAME}" == hdfml* || "${HOST_NAME}" == juwels* ]]; then
--- video_prediction_tools/env_setup/modules_train.sh
+++ video_prediction_tools/env_setup/modules_train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # __author__ = Bing Gong, Michael Langguth
-# __date__  = '2020_06_26'
+# __date__  = '2021_01_15'
 
 # This script loads the required modules for ambs on Juwels and HDF-ML.
 # Note that some other packages have to be installed into a venv (see create_env.sh and requirements.txt).
@@ -11,19 +11,28 @@ HOST_NAME=`hostname`
 echo "Start loading modules on ${HOST_NAME}..."
 echo "modules_train.sh is subject to: "
 echo "* preprocess_data_era5_step2_<exp_id>.sh"
-echo "* train_model_era5_<exp_id>.sh"
+echo "* train_model_era5_[booster_]<exp_id>.sh"
 echo "* visualize_postprocess_era5_<exp_id>.sh"
 
 module purge
 module use $OTHERSTAGES
-module load Stages/2019a
-module load GCC/8.3.0
-module load GCCcore/.8.3.0
-module load ParaStationMPI/5.2.2-1
-module load mpi4py/3.0.1-Python-3.6.8
-# serialized version of HDF5 is used since only this version is compatible with TensorFlow/1.13.1-GPU-Python-3.6.8
-module load h5py/2.9.0-serial-Python-3.6.8
-module load TensorFlow/1.13.1-GPU-Python-3.6.8
-module load cuDNN/7.5.1.10-CUDA-10.1.105
-module load netcdf4-python/1.5.0.1-Python-3.6.8
+if [[ "${HOST_NAME}" == jwlogin21* || "${HOST_NAME}" == jwlogin22* || "${HOST_NAME}" == jwlogin23* ]]; then
+  module load Stages/2020
+  module load UCX/1.8.1
+  module load GCC/9.3.0
+  module load OpenMPI/4.1.0rc1
+  echo "Scarlet we are in the first if"
+else
+  module load Stages/2019a
+  module load GCC/8.3.0
+  module load GCCcore/.8.3.0
+  module load ParaStationMPI/5.2.2-1
+  module load mpi4py/3.0.1-Python-3.6.8
+  # serialized version of HDF5 is used since only this version is compatible with TensorFlow/1.13.1-GPU-Python-3.6.8
+  module load h5py/2.9.0-serial-Python-3.6.8
+  module load TensorFlow/1.13.1-GPU-Python-3.6.8
+  module load cuDNN/7.5.1.10-CUDA-10.1.105
+  module load netcdf4-python/1.5.0.1-Python-3.6.8
+  echo "Scarlet, correct"
+fi
 
