14521875b31752ab001d533d7ea2d2730683a306
Michael <m.langguth@fz-juelich.de>
2021-02-04 11:13:19 +0100

    Minor change in print-statement of modules_train.sh.

--- video_prediction_tools/env_setup/modules_train.sh
+++ video_prediction_tools/env_setup/modules_train.sh
@@ -13,7 +13,6 @@ echo "Start loading modules on ${HOST_NAME}..."
 echo "modules_train.sh is subject to: "
 echo "* preprocess_data_era5_step2_<exp_id>.sh"
 echo "* train_model_era5_[booster_]<exp_id>.sh"
-echo "* visualize_postprocess_era5_<exp_id>.sh"
 
 module use $OTHERSTAGES
 if [[ "${HOST_NAME}" == jwlogin2[1-4]* || "${HOST_NAME}" == jwb* ]]; then
