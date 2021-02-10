bbcd10bf3bdddb05c6c849675e50214636b8fafd
Michael <m.langguth@fz-juelich.de>
2021-02-10 17:45:20 +0100

    Several bugfixes and refinements to config_training.py.

--- video_prediction_tools/config_runscripts/config_training.py
+++ video_prediction_tools/config_runscripts/config_training.py
@@ -32,7 +32,7 @@ class Config_Train(Config_runscript_base):
         self.model = None
         self.destination_dir = None
         # list of variables to be written to runscript
-        self.list_batch_vars = ["VIRT_ENV_NAME", "source_dir", "model", "exp_id", "destination_dir"]
+        self.list_batch_vars = ["VIRT_ENV_NAME", "source_dir", "model", "destination_dir"]
         # copy over method for keyboard interaction
         self.run_config = Config_Train.run_training
     #
@@ -43,23 +43,20 @@ class Config_Train(Config_runscript_base):
         Runs the keyboard interaction for Training
         :return: all attributes of class training are set
         """
+        method_name = Config_Train.run_training.__name__
 
         # decide which dataset is used
-        dset_type_req_str = "Enter the name of the dataset on which you want to train:\n"
+        dset_type_req_str = "Enter the name of the dataset on which you want to train:"
         dset_err = ValueError("Please select a dataset from the ones listed above.")
 
         self.dataset = Config_Train.keyboard_interaction(dset_type_req_str, Config_Train.check_dataset,
                                                          dset_err, ntries=2)
 
-        # now, we are also ready to set the correct name of the runscript template and the target
-        self.runscript_template = self.rscrpt_tmpl_prefix + self.dataset + self.suffix_template
-        self.runscript_target = self.rscrpt_tmpl_prefix + self.dataset + ".sh"
-
-        # get the source directory
         # get source dir (relative to base_dir_source!)
+        self.runscript_template = os.path.join(self.runscript_dir, "train_model_{0}{1}".format(self.dataset, self.suffix_template))
         source_dir_base = Config_Train.handle_source_dir(self, "preprocessedData")
 
-        expdir_req_str = "Choose a subdirectory listed above where the preprocessed TFrecords are located:\n"
+        expdir_req_str = "Choose a subdirectory listed above where the preprocessed TFrecords are located:"
         expdir_err = FileNotFoundError("Could not find any tfrecords.")
 
         self.source_dir = Config_Train.keyboard_interaction(expdir_req_str, Config_Train.check_expdir,
@@ -72,19 +69,19 @@ class Config_Train(Config_runscript_base):
         index = [idx for idx, s in enumerate(exp_dir_split) if self.dataset in s]
         if index == []:
             raise ValueError(
-                "tfrecords found under '{0}', but directory does not seem to reflect naming convention.".format(
-                    self.source_dir))
+                    "%{0}: tfrecords found under '{1}', but directory does not seem to reflect naming convention.".format(
+                    method_name, self.source_dir))
         exp_dir = exp_dir_split[index[0]]
 
         # get the model to train
-        model_req_str = "Enter the name of the model you want to train:\n"
+        model_req_str = "Enter the name of the model you want to train:"
         model_err     = ValueError("Please select a model from the ones listed above.")
 
         self.model = Config_Train.keyboard_interaction(model_req_str, Config_Train.check_model, model_err, ntries=2)
 
         # experimental ID
         # No need to call keyboard_interaction here, because the user can pass whatever we wants
-        self.exp_id = input("Enter your desired experimental id (will be extended by timestamp and username):\n")
+        self.exp_id = input("*** Enter your desired experimental id (will be extended by timestamp and username):\n")
 
         # also get current timestamp and user-name...
         timestamp = dt.datetime.now().strftime("%Y%m%dT%H%M%S")
@@ -92,39 +89,44 @@ class Config_Train(Config_runscript_base):
         # ... to construct final destination_dir and exp_dir_ext as well
         self.exp_id = timestamp +"_"+ user_name +"_"+ self.exp_id  # by convention, exp_id is extended by timestamp and username
 
-        base_dir   = get_variable_from_runscript("train_model_{0}_template.sh".format(self.dataset), "destination_dir")
+        # now, we are also ready to set the correct name of the runscript template and the target
+        self.runscript_target = "{0}{1}_{2}.sh".format(self.rscrpt_tmpl_prefix, self.dataset, self.exp_id)
+        
+        base_dir   = Config_Train.get_var_from_runscript(os.path.join(self.runscript_dir, self.runscript_template), "destination_dir")
         exp_dir_ext= os.path.join(exp_dir, self.model, self.exp_id)
-        self.destination_dir = os.path.join(base_dir, exp_dir, self.model, self.exp_id)
-
+        self.destination_dir = os.path.join(base_dir, "models", exp_dir, self.model, self.exp_id)
+        
         # sanity check (target_dir is unique):
         if os.path.isdir(self.destination_dir):
-            raise IsADirectoryError(self.destination_dir+" already exists! Make sure that it is unique.")
+            raise IsADirectoryError("%{0}: {1} already exists! Make sure that it is unique.".format(method_name, self.destination_dir))
 
         # create destination directory...
         os.makedirs(self.destination_dir)
 
         # Create json-file for data splitting
-        source_datasplit = os.path.join("..", "data_split", self.dataset, self.model, "datasplit.json")
+        source_datasplit = os.path.join("..", "data_split", "datasplit_template.json")
+        dest_datasplit = os.path.join(self.destination_dir, "data_split.json")
         # sanity check (default data_split json-file exists)
         if not os.path.isfile(source_datasplit):
-            raise FileNotFoundError("Could not find default data_split json-file '"+source_datasplit+"'")
+            raise FileNotFoundError("%{0}: Could not find default data_split json-file '{1}'".format(method_name, source_datasplit))
         # ...copy over json-file for data splitting...
-        os.system("cp "+source_datasplit+" "+self.destination_dir)
+        os.system("cp "+source_datasplit+" "+dest_datasplit)
         # ...and open vim after some delay
-        print("You may now configure the data splitting:\n")
+        print("*** Please configure the data splitting:")
         time.sleep(3)
         cmd_vim = os.environ.get('EDITOR', 'vi') + ' ' + os.path.join(self.destination_dir,"data_split.json")
         sp.call(cmd_vim, shell=True)
+        sp.call("sed -i '/^#/d' {0}".format(dest_datasplit), shell=True)
 
         # Create json-file for hyperparameters
         source_hparams = os.path.join("..","hparams", self.dataset, self.model, "model_hparams.json")
         # sanity check (default hyperparameter json-file exists)
         if not os.path.isfile(source_hparams):
-            raise FileNotFoundError("Could not find default hyperparameter json-file '"+source_hparams+"'")
+            raise FileNotFoundError("%{0}: Could not find default hyperparameter json-file '%{1}'".format(method_name, source_hparams))
         # ...copy over json-file for hyperparamters...
         os.system("cp "+source_hparams+" "+self.destination_dir)
         # ...and open vim after some delay
-        print("You may now configure the model hyperparameters:\n")
+        print("*** Please configure the model hyperparameters:")
         time.sleep(3)
         cmd_vim = os.environ.get('EDITOR', 'vi') + ' ' + os.path.join(self.destination_dir, "model_hparams.json")
         sp.call(cmd_vim, shell=True)
