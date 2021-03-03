bb9b636af96b8388e9e418a5235aefdc02dfa972
gong1 <b.gong@fz-juelich.de>
2021-03-03 15:25:07 +0100

    Modularized data extraction and add unitest

--- test/run_pytest.sh
+++ test/run_pytest.sh
@@ -16,10 +16,14 @@ fi
 
 
 
-#source ../video_prediction_tools/env_setup/modules_preprocess.sh
+source ../video_prediction_tools/env_setup/modules_preprocess.sh
+##Test data extraction
+
+ml Stages/2019a  GCC/8.3.0  ParaStationMPI/5.2.2-1 CDO/1.9.6
+python -m pytest  test_prepare_era5_data.py 
 ##Test for preprocess_step1
 #python -m pytest  test_process_netCDF_v2.py
-source ../video_prediction_tools/env_setup/modules_train.sh
+#source ../video_prediction_tools/env_setup/modules_train.sh
 #Test for process step2
 #python -m pytest test_data_preprocess_step2.py
 #python -m pytest test_era5_data.py
@@ -27,6 +31,6 @@ source ../video_prediction_tools/env_setup/modules_train.sh
 #First remove all the files in the test folder
 #rm /p/project/deepacf/deeprain/video_prediction_shared_folder/models/test/* 
 #python -m pytest test_train_model_era5.py
-python -m pytest test_vanilla_vae_model.py
+#python -m pytest test_vanilla_vae_model.py
 #python -m pytest test_visualize_postprocess.py
 #python -m pytest test_meta_postprocess.py
