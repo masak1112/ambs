8c6f3afecf29dda9e3159fe714f3895b4df0de1d
Michael <m.langguth@fz-juelich.de>
2021-03-19 15:00:51 +0100

    Remove handling of source_dir from config_postprocess since this now retrieved from the model's JSON-files (namely options.json).

--- video_prediction_tools/config_runscripts/config_postprocess.py
+++ video_prediction_tools/config_runscripts/config_postprocess.py
@@ -6,7 +6,6 @@ __date__ = "2021-02-01"
 
 # import modules
 import os, glob
-import json
 from model_modules.model_architectures import known_models
 from data_preprocess.dataset_options import known_datasets
 from config_utils import Config_runscript_base    # import parent class
@@ -31,8 +30,7 @@ class Config_Postprocess(Config_runscript_base):
         self.checkpoint_dir = None
         self.results_dir = None
         # list of variables to be written to runscript
-        self.list_batch_vars = ["VIRT_ENV_NAME", "source_dir", "results_dir",
-                                "checkpoint_dir", "model"]
+        self.list_batch_vars = ["VIRT_ENV_NAME", "results_dir", "checkpoint_dir", "model"]
         # copy over method for keyboard interaction
         self.run_config = Config_Postprocess.run_postprocess
     #
@@ -89,36 +87,11 @@ class Config_Postprocess(Config_runscript_base):
         base_dir, exp_dir_base, exp_dir = "/"+os.path.join(*cp_dir_split[:-4]), cp_dir_split[-3], cp_dir_split[-1]
         self.runscript_target = self.rscrpt_tmpl_prefix + self.dataset + exp_dir + ".sh"
 
-        # get the sequence length from the model hyperparameters
-        seq_length = Config_Postprocess.get_seq_length(self)
-
-        self.source_dir = Config_Postprocess.check_source(os.path.join(base_dir, "preprocessedData", exp_dir_base),
-                                                          seq_length)
+        # finally, set results_dir
         self.results_dir = os.path.join(base_dir, "results", exp_dir_base, self.model, exp_dir)
     #
     # -----------------------------------------------------------------------------------
     #
-    def get_seq_length(self):
-
-        method = Config_Postprocess.get_seq_length.__name__
-
-        if self.checkpoint_dir is None:
-            raise ValueError("%{0}: checkpoint_dir-attribute is still None".format(method))
-
-        model_hparams_js = os.path.join(self.checkpoint_dir, "model_hparams.json")
-        if not os.path.isfile(model_hparams_js):
-            raise FileNotFoundError("%{0}: Could not file JSON-file with model hyperpparameters named '{1}'"
-                                    .format(method, model_hparams_js))
-        else:
-            with open(model_hparams_js) as js_file:
-                js_data = json.load(js_file)
-                try:
-                    seq_length = js_data["sequence_length"]
-                except:
-                    raise ValueError("%{0}: Could not find sequence_length in '{1}'".format(method, model_hparams_js))
-
-        return seq_length
-
     @staticmethod
     # dataset used for postprocessing
     def check_dataset(dataset_name, silent=False):
@@ -213,27 +186,3 @@ class Config_Postprocess(Config_runscript_base):
         else:
             if not silent: print("Passed directory '{0}' does not exist!".format(checkpoint_dir))
         return status
-    #
-    # -----------------------------------------------------------------------------------
-    #
-    @staticmethod
-    def check_source(source_dir_in, seq_length):
-        """
-        Checks if TFrecord files (used for training) are available. These are used to construct the test-dataset
-        :param source_dir_in: input directory to be checked
-        :param seq_length: sequence_length of TFrecord-files in source_dir_in
-        :return: returns source_dir_in when check is passed successfully
-        """
-        real_dir = os.path.join(source_dir_in, "tfrecords_seq_len_{0}".format(seq_length))
-        if os.path.isdir(real_dir):
-            file_list = glob.glob(os.path.join(real_dir, "sequence*.tfrecords"))
-            if len(file_list) > 0:
-                pass
-            else:
-                raise FileNotFoundError("{0} does not contain any tfrecord-files.".format(real_dir))
-        else:
-            raise NotADirectoryError("Cannot find directory '{0}'.".format(real_dir))
-
-        return source_dir_in
-
-
