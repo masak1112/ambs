66e6f4b3c1af0965aff7e489c1f8ddbf02c52bcc
Michael <m.langguth@fz-juelich.de>
2022-01-11 16:43:32 +0100

    Further work with included get_year_seqs-function which will not be used later for the sake of using the year_month_list-distributor of PyStager instead of the date-distributor.

--- video_prediction_tools/data_preprocess/preprocess_data_step1.py
+++ video_prediction_tools/data_preprocess/preprocess_data_step1.py
@@ -6,11 +6,12 @@ __author__ = "Michael Langguth"
 __date__ = "2021-12-13"
 
 import os, glob
-from typing import List, Union
+from typing import List, Union, get_args
 import shutil
 import subprocess as sp
+import numpy as np
 import datetime as dt
-from general_utils import check_str_in_list, get_path_component
+from general_utils import check_str_in_list, get_path_component, isw
 from netcdf_datahandling import NetcdfUtils
 from pystager_utils import PyStager
 
@@ -22,15 +23,19 @@ class Preprocess_ERA5_data(object):
 
     cls_name = "Preprocess_ERA5_data"
 
-    def __init__(self, dirin, dirout, varnames, vartypes, coord_sw, nyx, years):
+    def __init__(self, dirin, dirout, varnames, vartypes, coord_sw, nyx, years, months = "all",
+                 lon_intv: List = (0., 360.), lat_inv: List = (-90., 90.), dx: float = 0.3):
         """
         This script performs several sanity checks and sets the class attributes accordingly.
         :param dirin: directory to the ERA5 reanalysis data
         :param dirout: directory where the output data will be saved
         :param varnames: list of variables to be extracted
         :param vartypes: type of variable matching varnames
-        :param coord_sw: latitude and longitude of south-western corner defining target domain
+        :param coord_sw: latitude [°N] and longitude [°E] of south-western corner defining target domain
         :param nyx: number of grid points in latitude and longitude direction for target domain
+        :param lon_intv: interval of the longitude values for ERA5 data (adapt if required)
+        :param lat_intv: interval of the latitude values for ERA5 data (adapt if required)
+        :param dx: grid spacing of regular, spherical grid onto which the ERA5 data is provided (adapt if required)
         """
         method = Preprocess_ERA5_data.__init__.__name__
 
@@ -39,26 +44,98 @@ class Preprocess_ERA5_data(object):
             raise NotADirectoryError("%{0}: Output directory does not exist.".format(method))
 
         self.dirin, self.years = self.check_dirin(dirin, years)
+        self.months = self.get_months(months)
         self.varnames, self.vartypes = self.check_varnames(varnames, vartypes, dirin)
+        # some basic grid information
+        self.lat_intv, self.lon_intv = lat_inv, lon_intv
+        self.lon_intv[1] -= np.abs(dx)
+        self.dx = dx
+        # check provided data for regional slicing
         self.lat_bounds, self.lon_bounds = self.check_coords(coord_sw, nyx)
 
-    def check_coords(self, coords_sw: List, nyx: List):
+        # initialize PyStager
+        era5_pystager = PyStager(self.preprocess_worker, "year_month_list", nmax_warn=5)
 
+    def __call__(self):
+
+        for seq_key in years_dict:
+            era5_pystager.setup(self.years, self.months)
+            era5_pystager.run(self.dirin, self.dirout, self.varnames, self.vartypes, self.lat_bounds, self.lon_bounds)
+
+    def check_coords(self, coords_sw: List, nyx: List):
+        """
+        Check arguments for defining target domain and return bounding coordinates.
+        Note: Currently, crossing of the zero-meridian is not supported!
+        :param coords_sw: latitude [°N] and longitude [°E] of south-western corner defining target domain
+        :param nyx: number of grid points in latitude and longitude direction for target domain
+        :return: two tuples of latitude and longitude boundaries of target domain, respectively
+        """
         method = Preprocess_ERA5_data.check_coords.__name__
 
-        fexample = os.path.join(self.dirin, self.years[0], "01", "{0}010100_sf.grb".format(self.years[0]))
-        fexample_nc = fexample.replace(".grb", ".nc")
+        if self.dx < 0:
+            print("%{0}: Grid spacing should be positive. Change negative value to positive one.".format(method))
+            self.dx = np.abs(self.dx)
 
-        if not os.path.isfile(fexample):
-            raise FileNotFoundError("%{0}: Could not find example file '{1}' for retrieving coordinate data."
-                                    .format(method, fexample))
+        if not isw(coords_sw[0], self.lat_intv):
+            raise ValueError("%{0}: Latitude of south-western domain corner {1:.2f}".format(method, coords_sw[0]) +
+                             "is not within expected [{0:.1f}°N, {1:.f}°N]".format(self.lat_intv[0], self.lat_intv[1]))
 
-        cmd = "cdo --eccodes -f nc copy {0} {1}".format(fexample, fexample_nc)
-        sp.call(cmd, shell=True)
+        if not isw(coords_sw[1], self.lon_intv):
+            raise ValueError("%{0}: Longitude of south-western domain corner {1:.2f}".format(method, coords_sw[1]) +
+                             "is not within expected [{0:.1f}°E, {1:.f}°E]".format(self.lon_intv[0], self.lon_intv[1]))
 
-        dexample = NetcdfUtils(fexample_nc)
+        coords_ne = coords_sw[0] + (nyx[0] - 1)*self.dx, coords_sw[1] + (nyx[1] - 1)*self.dx
 
-        lat, lon = dexample["lon"].values, dexample["lat"].values
+        if not isw(coords_ne[0], self.lat_intv):
+            raise ValueError("%{0}: Latitude of north-eastern domain corner {1:.2f}".format(method, coords_ne[0]) +
+                             "is not within expected [{0:.1f}°N, {1:.f}°N]. Adapt nyx-argument."
+                             "".format(self.lat_intv[0], self.lat_intv[1]))
+
+        if not isw(coords_ne[1], self.lon_intv):
+            raise ValueError("%{0}: Longitude of north-eastern domain corner {1:.2f}".format(method, coords_ne[1]) +
+                             "is not within expected [{0:.1f}°E, {1:.f}°E]. Adapt nyx-argument."
+                             .format(self.lon_intv[0], self.lon_intv[1]))
+
+        # ML: Approach with automatic check from file -> deprecated (i.e. the user should know his data)
+        # fexample = os.path.join(self.dirin, self.years[0], "01", "{0}010100_sf.grb".format(self.years[0]))
+        # fexample_nc = fexample.replace(".grb", ".nc")
+        #
+        # if not os.path.isfile(fexample):
+        #     raise FileNotFoundError("%{0}: Could not find example file '{1}' for retrieving coordinate data."
+        #                             .format(method, fexample))
+        #
+        # cmd = "cdo --eccodes -f nc copy {0} {1}".format(fexample, fexample_nc)
+        # sp.call(cmd, shell=True)
+        #
+        # dexample = NetcdfUtils(fexample_nc)
+        #
+        # lat, lon = dexample["lon"].values, dexample["lat"].values
+        return [coords_sw[0], coords_ne[0]], [coords_sw[1], coords_ne[1]]
+
+    def get_year_seqs(self):
+
+        years_arr = np.sort(np.asarray(self.years))
+        nyears = np.size(years_arr)
+
+        # initialize dictionary and...
+        year_seqs = {"seq_0": [years_arr[0], None]}
+        # ..counters
+        c = 1
+        ii = 0
+
+        while c <= nyears - 1:
+            if years_arr[c-1] + 1 != years_arr[c]:
+                year_seqs["seq_{0:d}".format(ii)][1] = years_arr[c - 1]
+                ii += 1
+                year_seqs["seq_{0:d}".format(ii)] = [years_arr[c], None]
+            c += 1              # increment loop counter
+
+        # set final element of last dictionary value (still None)
+        year_seqs["seq_{0:d}".format(ii)][1] = years_arr[-1]
+
+        return year_seqs
+
+
 
     @staticmethod
     def check_dirin(dirin: str, years: str_or_List):
@@ -94,6 +171,36 @@ class Preprocess_ERA5_data(object):
         return dirin, years
 
     @staticmethod
+    def get_months(months):
+
+        method = Preprocess_ERA5_data.get_months.__name__
+
+        assert isinstance(months, get_args(str_or_List)), \
+               "%{0}: months must be either a list of months or a (known) string.".format(method)
+
+        if isinstance(months, list):
+            if np.all([isinstance(x, int) & isw(x, [1,12]) for x in months]):
+                month_list = months
+            else:
+                raise ValueError("%{0}: Not all elements of months can serve as month-integers".format(method) +
+                                 "(only values between 1 and 12 can be passed).")
+        elif months == "DJF":
+            month_list = [1, 2, 12]
+        elif months == "MAM":
+            month_list = [3, 4, 5]
+        elif months == "JJA":
+            month_list = [6, 7, 8]
+        elif months == "SON":
+            month_list = [9, 10, 11]
+        elif months == "all":
+            month_list = list(np.range(1,13))
+        else:
+            raise ValueError("%{0}: months-argument cannot be converted to list of months (see doc-string)"
+                             .format(method))
+
+        return month_list
+
+    @staticmethod
     def check_varnames(varnames: str_or_List, vartypes: str_or_List, datadir: str):
         """
         Check if all variables can be found in an exemplary datafile stored under datadir
@@ -122,7 +229,7 @@ class Preprocess_ERA5_data(object):
             yr, mm = get_path_component(datadir, -2), get_path_component(datadir, -1)
             f2check = os.path.join(datadir, "{0}{1}0100_{2}.grb".format(yr, mm, vartype))
 
-            stat = Preprocess_ERA5_data.check_var_in_grib(f2check, vars2check, labort=True)
+            _ = Preprocess_ERA5_data.check_var_in_grib(f2check, vars2check, labort=True)
 
         return varnames, vartypes
 
@@ -151,38 +258,38 @@ class Preprocess_ERA5_data(object):
 
         return stat
 
-    @staticmethod
-    def get_ERA5_coords(era5_file):
-        """
-        Retrieves latitude and longitude coordinates from ERA5-file
-        :param era5_file: Path to ERA5-file. Can be either a netCDF- or grib2-file
-        :return: numpy-arrays of latitude and longitude coordinates
-        """
-
-        method = Preprocess_ERA5_data.get_ERA5_coords.__name__
-
-        if not os.path.isfile(era5_file):
-            raise FileNotFoundError("%{0}: Could not find example file '{1}' for retrieving coordinate data."
-                                    .format(method, era5_file))
-
-        if era5_file.endswith(".grb"):
-            era5_file_nc = era5_file.replace(".grb", ".nc")
-            cmd = "cdo --eccodes -f nc copy {0} {1}".format(era5_file, era5_file_nc)
-            sp.call(cmd, shell=True)
-        elif era5_file.endswith(".nc"):
-            era5_file_nc = era5_file
-        else:
-            raise ValueError("%{0}: '{1}' must be either a grib2 or netCDF-file.".format(method, era5_file))
-
-        data_era5 = NetcdfUtils(era5_file_nc)
-
-        try:
-            lat, lon = data_era5.coords["lat"], data_era5["lon"]
-        except Exception as err:
-            print("%{0}: Failed to retrieve lat and lon from file '{1}'".format(method, era5_file_nc))
-            raise err
-
-        return lat.values, lon.values
+    # deprecated (see note under check_coords-methods above)
+    # @staticmethod
+    # def get_ERA5_coords(era5_file):
+    #     """
+    #     Retrieves latitude and longitude coordinates from ERA5-file
+    #     :param era5_file: Path to ERA5-file. Can be either a netCDF- or grib2-file
+    #     :return: numpy-arrays of latitude and longitude coordinates
+    #     """
+    #     method = Preprocess_ERA5_data.get_ERA5_coords.__name__
+    #
+    #     if not os.path.isfile(era5_file):
+    #         raise FileNotFoundError("%{0}: Could not find example file '{1}' for retrieving coordinate data."
+    #                                 .format(method, era5_file))
+    #
+    #     if era5_file.endswith(".grb"):
+    #         era5_file_nc = era5_file.replace(".grb", ".nc")
+    #         cmd = "cdo --eccodes -f nc copy {0} {1}".format(era5_file, era5_file_nc)
+    #         sp.call(cmd, shell=True)
+    #     elif era5_file.endswith(".nc"):
+    #         era5_file_nc = era5_file
+    #     else:
+    #         raise ValueError("%{0}: '{1}' must be either a grib2 or netCDF-file.".format(method, era5_file))
+    #
+    #     data_era5 = NetcdfUtils(era5_file_nc)
+    #
+    #     try:
+    #         lat, lon = data_era5.coords["lat"], data_era5["lon"]
+    #     except Exception as err:
+    #         print("%{0}: Failed to retrieve lat and lon from file '{1}'".format(method, era5_file_nc))
+    #         raise err
+    #
+    #     return lat.values, lon.values
 
 
 
