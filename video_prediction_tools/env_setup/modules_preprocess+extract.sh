aca21f9e0129d7ac2bf8108f6df8dc97a18f8631
Michael <m.langguth@fz-juelich.de>
2022-02-07 08:49:34 +0100

    Add new version of modules_preprocess+extract.sh which was somehow lost in previous commits.

--- video_prediction_tools/env_setup/modules_preprocess+extract.sh
+++ video_prediction_tools/env_setup/modules_preprocess+extract.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 
-# __author__ = Bing Gong, Michael Langguth
-# __date__  = '2020_06_26'
+# __author__ = Michael Langguth
+# __date__  = '2022_02_01'
 
-# This script loads the required modules for ambs on Juwels and HDF-ML.
-# Note that some other packages have to be installed into a venv (see create_env.sh and requirements.txt).
+# This script loads the required modules for AMBS on JSC's HPY_systems (HDF-ML, Juwels Cluster and Juwels Booster).
+# Further Python-packages may be installed in the virtual environment created by create_env.sh
+# (see also requirements.txt).
 
-HOST_NAME=`hostname`
+HOST_NAME=$(hostname)
 
 echo "Start loading modules on ${HOST_NAME} required for preprocessing..."
-echo "modules_preprocess.sh is subject to: "
+echo "modules_preprocess+extract.sh is used for: "
+echo "* data_extraction_era5.sh"
 echo "* preprocess_data_era5_step1.sh"
+echo "* generate_runscript.py"
 
 module purge
-module use $OTHERSTAGES
-ml Stages/2019a
-ml GCC/8.3.0
-ml ParaStationMPI/5.2.2-1
+module use "$OTHERSTAGES"
+ml Stages/2020
+ml GCC/10.3.0
+ml GCCcore/.10.3.0
+ml ParaStationMPI/5.4.10-1
+ml mpi4py/3.0.3-Python-3.8.5
 ml mpi4py/3.0.1-Python-3.6.8
-# serialized version is not available on HFML
-# see https://gitlab.version.fz-juelich.de/haf/Wiki/-/wikis/HDF-ML%20System
-if [[ "${HOST_NAME}" == hdfml* ]]; then
-  ml h5py/2.9.0-serial-Python-3.6.8
-elif [[ "${HOST_NAME}" == juwels* ]]; then
-  ml h5py/2.9.0-Python-3.6.8
-fi
-ml SciPy-Stack/2019a-Python-3.6.8
-ml scikit/2019a-Python-3.6.8
-ml netcdf4-python/1.5.0.1-Python-3.6.8
+ml h5py/2.10.0-Python-3.8.5
+ml netcdf4-python/1.5.4-Python-3.8.5
+ml SciPy-Stack/2021-Python-3.8.5
+ml scikit/2021-Python-3.8.5
+ml CDO/2.0.0rc3
 
 # clean up if triggered via script argument
 if [[ $1 == purge ]]; then
