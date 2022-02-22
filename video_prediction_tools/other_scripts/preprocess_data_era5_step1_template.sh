6a5437c21ada15e4fec01739fe5cc479bb91ad0a
Bing Gong <b.gong@fz-juelich.de>
2022-02-22 15:21:21 +0100

    update preprocess step 1 template

--- video_prediction_tools/other_scripts/preprocess_data_era5_step1_template.sh
+++ video_prediction_tools/other_scripts/preprocess_data_era5_step1_template.sh
@@ -6,15 +6,18 @@ VIRT_ENV_NAME=venv_test
 echo "Activating virtual environment..."
 source ../virtual_envs/${VIRT_ENV_NAME}/bin/activate
 
-# select years and variables for dataset and define target domain 
-years=( "2016" )
-variables=( "var167"  )
+#select years and variables for dataset and define target domain 
+years=( "2007" )
+variables=( "2t"  )
 sw_corner=( 10 20)
-nyx=( 24 24 )
+nyx=( 40 40 )
 
+#your source dir and target dir
 source_dir=/home/b.gong/data_era5
 destination_dir=/home/b.gong/preprocessed_data
-n_nodes=2
+
+#The number of nodes should be equal to the number of 1 preprcessed folder plus 1
+n_nodes=3
 
 for year in "${years[@]}"; do
   echo "start preprocessing data for year ${year}"
