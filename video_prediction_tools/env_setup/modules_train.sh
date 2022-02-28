7d5eda83fc2effec44d07bf4a178348b7939b721
Michael <m.langguth@fz-juelich.de>
2022-02-28 23:33:18 +0100

    Add dummy file for modules_train.sh to repo.

---
+++ video_prediction_tools/env_setup/modules_train.sh
@@ -0,0 +1,3 @@
+# Please populate this file when you run on an HPC-system without singularity containers
+# You may refer to the file modules_preprocess+extract.sh as blue print which serves for the data extraction and the
+# preprocessing step 1 on JSC's HPC_system.
