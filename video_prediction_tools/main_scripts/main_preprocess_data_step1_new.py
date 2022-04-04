d15bbd5afe77a35feefdb25ff373b9f7f3d1b7db
Michael <m.langguth@fz-juelich.de>
2022-04-04 17:48:50 +0200

    Parse variables as dictionary-like strings to main_preprocess_data_step1.py.

--- video_prediction_tools/main_scripts/main_preprocess_data_step1_new.py
+++ video_prediction_tools/main_scripts/main_preprocess_data_step1_new.py
@@ -6,6 +6,7 @@ and performs parallelization with PyStager.
 __email__ = "m.langguth@fz-juelich.de"
 __author__ = "Michael Langguth"
 
+import json as js
 import argparse
 from data_preprocess.preprocess_data_step1 import Preprocess_ERA5_data
 
@@ -20,8 +21,9 @@ def main():
     parser.add_argument("--years", "-y", nargs="+", dest="years", help="Years of data to be processed.")
     parser.add_argument("--months", "-m", nargs="+", dest="months",
                         help="Months of data. Can also be 'all' or season-strings, e.g. 'DJF'.")
-    parser.add_argument("--variables", "-v", dest="vars", nargs="+", default=["2t"],
-                        help="Variables to be processed.")
+    parser.add_argument("--variables", "-v", dest="vars_dict", type=js.loads, default='{"2t": "sfc"}',
+                        help="Dictionary-like string to parse variable names (keys) together with " + \
+                             "variable types (values).")
     parser.add_argument("--sw_corner", "-swc", dest="sw_corner", nargs="+",
                         help="Defines south-west corner of target domain (lat, lon)=(-90..90, 0..360)")
     parser.add_argument("--nyx", "-nyx", dest="nyx", nargs="+",
@@ -29,8 +31,10 @@ def main():
 
     args = parser.parse_args()
 
+    print(args.vars_dict)
+
     # initialize preprocessing instance...
-    era5_preprocess = Preprocess_ERA5_data(args.source_dir, args.destination_dir, args.vars, args.sw_corner,
+    era5_preprocess = Preprocess_ERA5_data(args.source_dir, args.destination_dir, args.vars_dict, args.sw_corner,
                                            args.nyx, args.years, args.months)
 
     # ...and run it
