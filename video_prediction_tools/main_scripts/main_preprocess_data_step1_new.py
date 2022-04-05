56c7b52c3c41ceea1d164f468d0e708c822c47cf
Michael <m.langguth@fz-juelich.de>
2022-04-05 08:29:44 +0200

    Add defaults and flag for required arguments to main_preprocess_data_step1_new.py.

--- video_prediction_tools/main_scripts/main_preprocess_data_step1_new.py
+++ video_prediction_tools/main_scripts/main_preprocess_data_step1_new.py
@@ -14,24 +14,27 @@ from data_preprocess.preprocess_data_step1 import Preprocess_ERA5_data
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--source_dir", "-src_dir", dest="source_dir", type=str,
+    parser.add_argument("--source_dir", "-src_dir", dest="source_dir", type=str, required=True,
                         help="Top-level directory where ERA5 grib-files are located under <year>/<month>.")
-    parser.add_argument("--destination_dir", "-dest_dir", dest="destination_dir", type=str,
+    parser.add_argument("--destination_dir", "-dest_dir", dest="destination_dir", type=str, required=True,
                         help="Destination directory where the netCDF-files will be stored")
-    parser.add_argument("--years", "-y", nargs="+", dest="years", type=int, help="Years of data to be processed.")
-    parser.add_argument("--months", "-m", nargs="+", dest="months",
+    parser.add_argument("--years", "-y", nargs="+", dest="years", type=int, required=True,
+                        help="Years of data to be processed.")
+    parser.add_argument("--months", "-m", nargs="+", dest="months", default="all",
                         help="Months of data. Can also be 'all' or season-strings, e.g. 'DJF'.")
     parser.add_argument("--variables", "-v", dest="vars_dict", type=js.loads, default='{"2t": "sfc"}',
-                        help="Dictionary-like string to parse variable names (keys) together with " + \
+                        help="Dictionary-like string to parse variable names (keys) together with " +
                              "variable types (values).")
-    parser.add_argument("--sw_corner", "-swc", dest="sw_corner", nargs="+",
+    parser.add_argument("--sw_corner", "-swc", dest="sw_corner", nargs="+", default=(38.4, 0.),
                         help="Defines south-west corner of target domain (lat, lon)=(-90..90, 0..360)")
-    parser.add_argument("--nyx", "-nyx", dest="nyx", nargs="+", type=int,
+    parser.add_argument("--nyx", "-nyx", dest="nyx", nargs="+", type=int, default=(56, 92),
                         help="Number of grid points in zonal and meridional direction.")
 
     args = parser.parse_args()
 
-    print(args.vars_dict)
+    # print parsed arguments
+    for key in args.keys():
+        print("{0}: {1}".format(key, args[key]))
 
     # initialize preprocessing instance...
     era5_preprocess = Preprocess_ERA5_data(args.source_dir, args.destination_dir, args.vars_dict, args.sw_corner,
