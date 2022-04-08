53e217b5cd25e1ba75f0351de20f4f280b01d622
BING GONG <b.gong@fz-juelich.de>
2022-04-08 10:53:20 +0200

    update unitest for era5 data

--- test/test_era5_data.py
+++ test/test_era5_data.py
@@ -13,6 +13,7 @@ mode = "val"
 
 
 @pytest.fixture(scope="module")
+
 def era5_dataset_case1():
     return ERA5Dataset(input_dir=input_dir, datasplit_config=datasplit_config, hparams_dict_config=hparams_dict_config,
                  mode="train", seed=1234, nsamples_ref=1000)
@@ -28,11 +29,11 @@ def test_get_filenames_from_datasplit(era5_dataset_case1):
     era5_dataset_case1.get_filename_from_datasplit()
     flname="era5_vars4ambs_201801.nc"
     check = flname in era5_dataset_case1.filenames
-    assert check  == True
+    assert check == True
 
-def test_load_data_from_nc(era5_dataset_case1):
-    era5_dataset_case1.load_data_from_nc()
-    ds = xr.open_mfdataset(era5_dataset_case1.filenames)
+# def test_load_data_from_nc(era5_dataset_case1):
+#     era5_dataset_case1.load_data_from_nc()
+#     ds = xr.open_mfdataset(era5_dataset_case1.filenames)
 
 
 
