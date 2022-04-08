cc72ecf013ef87b00f1aa1d3613064d945c4ca33
Michael <m.langguth@fz-juelich.de>
2022-04-08 08:09:51 +0200

    Create new template for ERA5 data extraction on HPC-systems.

--- video_prediction_tools/HPC_scripts/era5_data_extraction_template.sh
+++ video_prediction_tools/HPC_scripts/era5_data_extraction_template.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks=13
 ##SBATCH --ntasks-per-node=12
 #SBATCH --cpus-per-task=1
-#SBATCH --output=DataPreprocess_era5_step1-out.%j
-#SBATCH --error=DataPreprocess_era5_step1-err.%j
+#SBATCH --output=DataExtraction_era5_step1-out.%j
+#SBATCH --error=DataExtraction_era5_step1-err.%j
 #SBATCH --time=04:20:00
 #SBATCH --gres=gpu:0
 #SBATCH --partition=batch
@@ -36,24 +36,19 @@ source ../env_setup/modules_preprocess+extract.sh
 
 
 # select years and variables for dataset and define target domain 
-years=( "2015" )
-variables=( "t2" "t2" "t2" )
-sw_corner=( -999.9 -999.9)
-nyx=( -999 -999 )
+years=( 2017 )
+months=( "all" )
+var_dict='{"2t": {"sf": ""}, "tcc": {"sf": ""}, "t": {"ml": "p85000."}}'
+sw_corner=(38.4 0.0)
+nyx=(56 92)
 
 # set some paths
 # note, that destination_dir is adjusted during runtime based on the data
 source_dir=/my/path/to/extracted/data/
 destination_dir=/my/path/to/pickle/files
 
-# execute Python-scripts
-for year in "${years[@]}"; do
-  echo "start preprocessing data for year ${year}"
-	srun python ../main_scripts/main_preprocess_data_step1.py \
-        --source_dir ${source_dir} --destination_dir ${destination_dir} --years "${year}" \
-       	--vars "${variables[0]}" "${variables[1]}" "${variables[2]}" \
-       	--sw_corner "${sw_corner[0]}" "${sw_corner[1]}" --nyx "${nyx[0]}" "${nyx[1]}"
-done
+# execute Python-script
+srun python ../main_scripts/main_era5_data_extraction.py -src_dir "${source_dir}" \
+     -dest_dir "${destination_dir}" -y "${years[@]}" -m "${months[@]}" \
+     -swc "${sw_corner[@]}" -nyx "${nyx[@]}" -v "${var_dict}"
 
-
-#srun python ../../workflow_parallel_frame_prediction/DataPreprocess/mpi_split_data_multi_years.py --destination_dir ${destination_dir} --varnames T2 MSL gph500    
