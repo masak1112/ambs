ee133cf81a0cda57c335ad061a8241bdb56a8c05
BING GONG <b.gong@fz-juelich.de>
2023-01-12 13:55:30 -0500

    update weatherbenchma model

--- video_prediction_tools/model_modules/video_prediction/models/weatherBench3Dcnn.py
+++ video_prediction_tools/model_modules/video_prediction/models/weatherBench3Dcnn.py
@@ -7,122 +7,82 @@ __author__ = "Bing Gong"
 __date__ = "2021-04-13"
 
 import tensorflow as tf
-from tensorflow.contrib.training import HParams
 from model_modules.video_prediction.layers import layer_def as ld
-from model_modules.video_prediction.losses import  *
+from model_modules.video_prediction.losses import l1_loss
+from .our_base_model import BaseModels
 
-class WeatherBenchModel(object):
+class WeatherBenchModel(BaseModels):
 
-    def __init__(self, hparams_dict=None, mode="train",**kwargs):
+    filters = [64, 64, 64, 64, 2]
+    kernels = [5, 5, 5, 5, 5]
+
+    def __init__(self, hparams_dict_config: dict=None, mode:str="train", **kwargs):
         """
-        This is class for building weahterBench architecture by using updated hparameters
+        This is class for building weatherBench architecture by using updated hparameters
         args:
-             mode        :str, "train" or "val", side note: mode may not be used in the convLSTM, but this will be a useful argument for the GAN-based model
-             hparams_dict: dict, the dictionary contains the hparaemters names and values
+             mode         :"train" or "val", side note: mode may not be used in the convLSTM,
+                          this will be a useful argument for the GAN-based model
+             hparams_dict :The dictionary contains the hyper-parameters names and values
         """
-        self.hparams_dict = hparams_dict
-        self.mode = mode
-        self.hparams = self.parse_hparams()
-        self.learning_rate = self.hparams.lr
-        self.filters = self.hparams.filters
-        self.kernels = self.hparams.kernels
-        self.max_epochs = self.hparams.max_epochs
-        self.batch_size = self.hparams.batch_size
-        self.outputs = {}
-        self.total_loss = None
+        super().__init__(hparams_dict_config, mode)
 
-    def get_default_hparams(self):
-        return HParams(**self.get_default_hparams_dict())
 
-    def parse_hparams(self):
+    def parse_hparams(self, hparams):
         """
-        Parse the hparams setting to ovoerride the default ones
+        Obtain the hyper-parameters from the dict to the class variables
         """
+        try:
+            self.context_frames = self.hparams.context_frames
+            self.max_epochs = self.hparams.max_epochs
+            self.batch_size = self.hparams.batch_size
+            self.shuffle_on_val = self.hparams.shuffle_on_val
+            self.loss_fun = self.hparams.loss_fun
+            self.learning_rate = self.hparams.lr
+            self.sequence_length = self.hparams.sequence_length
+        except Exception as error:
+           print("error: {}".format(error))
+           raise ValueError("Method %{}: the hyper-parameter dictionary "
+                            "must include parameters above")
 
-        parsed_hparams = self.get_default_hparams().override_from_dict(self.hparams_dict or {})
-        return parsed_hparams
-
-
-    def get_default_hparams_dict(self):
-        """
-        The function that contains default hparams
-        Returns:
-            A dict with the following hyperparameters.
-            context_frames  : the number of ground-truth frames to pass in at start.
-            max_epochs      : the number of epochs to train model
-            lr              : learning rate
-            loss_fun        : the loss function
-            filters         : list contains the filters of each convolutional layer
-            kernels         : list contains the kernels size for each convolutional layer
-            """
-        hparams = dict(
-            sequence_length =13,
-            context_frames =1,
-            max_epochs = 20,
-            batch_size = 40,
-            lr = 0.001,
-            shuffle_on_val= True,
-            filters = [64, 64, 64, 64, 3],
-            kernels = [5, 5, 5, 5, 5]
-        )
-        return hparams
-
-
-    def build_graph(self, x):
-        self.is_build_graph = False
-        self.x = x["images"]
-
-        self.global_step = tf.train.get_or_create_global_step()
-        original_global_variables = tf.global_variables()
-
-        # Architecture
-        x_hat = self.build_model(self.x[:,0,:, :, :],self.filters, self.kernels)
+    def get_loss(self, x: tf.Tensor, x_hat: tf.Tensor):
         # Loss
-        
-        self.total_loss = l1_loss(self.x[:,1,:, :,:], x_hat[:,:,:,:])
+        total_loss = l1_loss(x[:, 1, :, :, :], x_hat[:, :, :, :])
+        return total_loss
 
-        # Optimizer
-        self.train_op = tf.train.AdamOptimizer(
-            learning_rate = self.learning_rate).minimize(self.total_loss, global_step = self.global_step)
+    def optimizer(self, total_loss):
+        return tf.train.AdamOptimizer(
+            learning_rate = self.learning_rate).minimize(total_loss,
+                                                         global_step =
+                                                         self.global_step)
 
-        # outputs
-        self.outputs["total_loss"] = self.total_loss
-       
-        # inferences
-        if self.mode == "test":
-            self.outputs["gen_images"] = self.forecast(self.x, 12, self.filters, self.kernels)
-        else:
-            self.outputs["gen_images"] = x_hat
-
-        # Summary op
-        tf.summary.scalar("total_loss", self.total_loss)
-        self.summary_op = tf.summary.merge_all()
-        global_variables = [var for var in tf.global_variables() if var not in original_global_variables]
-        self.saveable_variables = [self.global_step] + global_variables
-        self.is_build_graph = True
-        return self.is_build_graph
-
-
-    def build_model(self, x, filters, kernels):
+    def build_model(self, x):
         """Fully convolutional network"""
-        idx = 0 
+        x = x[:, 0, :, :, :]
+
+        _idx = 0
         for f, k in zip(filters[:-1], kernels[:-1]):
-            with tf.variable_scope("conv_layer_"+str(idx),reuse=tf.AUTO_REUSE):
-                x = ld.conv_layer(x, kernel_size=k, stride=1, num_features=f, idx="conv_layer_"+str(idx) , activate="leaky_relu")
-            idx += 1
-        with tf.variable_scope("Conv_last_layer",reuse=tf.AUTO_REUSE):
-            output = ld.conv_layer(x, kernel_size=kernels[-1], stride=1, num_features=filters[-1], idx="Conv_last_layer", activate="linear")
+            with tf.variable_scope("conv_layer_"+str(_idx), reuse=tf.AUTO_REUSE):
+                x = ld.conv_layer(x, kernel_size=k, stride=1,
+                                  num_features=f,
+                                  idx="conv_layer_"+str(_idx),
+                                  activate="leaky_relu")
+            _idx += 1
+        with tf.variable_scope("conv_last_layer", reuse=tf.AUTO_REUSE):
+            output = ld.conv_layer(x, kernel_size=kernels[-1],
+                                   stride=1, num_features=filters[-1],
+                                   idx="conv_last_layer", activate="linear")
+
         return output
 
 
-    def forecast(self, x, forecast_time, filters, kernels):
+    def forecast(self, x, forecast_time):
         x_hat = []
 
         for i in range(forecast_time):
             if i == 0:
-                x_pred = self.build_model(x[:,i,:, :,:],filters,kernels)
+                x_pred = self.build_model(x[:, i, :, :, :], filters, kernels)
             else:
-                x_pred = self.build_model(x_pred,filters,kernels)
+                x_pred = self.build_model(_x_pred, _filters, kernels)
             x_hat.append(x_pred)
 
         x_hat = tf.stack(x_hat)
