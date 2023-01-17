9e172bea2038d1965e8cd97aab632facb3d1caca
gong1 <b.gong@fz-juelich.de>
2023-01-17 17:56:30 +0100

    remove the useless model architecture

--- video_prediction_tools/model_modules/model_architectures.py
+++ video_prediction_tools/model_modules/model_architectures.py
@@ -1,12 +1,9 @@
 def known_models():
     """
     An auxilary function
-    ours_vae_l1 and ours_gan are from savp papers
     :return: dictionary of known model architectures
     """
     model_mappings = {
-        'ground_truth': 'GroundTruthVideoPredictionModel',
-        'savp': 'SAVPVideoPredictionModel',
         'convLSTM': 'VanillaConvLstmVideoPredictionModel',
         'convLSTM_gan': "ConvLstmGANVideoPredictionModel",
         'weatherBench': 'WeatherBenchModel'
